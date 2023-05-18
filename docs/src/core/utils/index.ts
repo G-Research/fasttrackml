22ff52ab2fd7a269f1a077ee62006551fb65a960
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2023-05-18 17:19:49 +0200

    adding comment + using array of assignees

--- docs/src/core/utils/index.ts
+++ docs/src/core/utils/index.ts
@@ -2,11 +2,14 @@ const openInNewTab = (url: string) => {
     window.open(url, '_blank', 'noreferrer');
 };
 
-export const openNewIssue = (base_url: string, title: string = "", body: string = "", assignee: string = "", labels: string[] = []) => {
+export const openNewIssue = (base_url: string, title: string = "", body: string = "", assignees: string[] = [], labels: string[] = []) => {
+    /**
+     * https://docs.github.com/en/github-ae@latest/issues/tracking-your-work-with-issues/creating-an-issue
+     */
     const url = new URL(base_url);
     url.searchParams.append("title", title);
     url.searchParams.append("body", body);
-    url.searchParams.append("assignee", assignee);
+    assignees.forEach(assignee => url.searchParams.append("assignees", assignee));
     labels.forEach(label => url.searchParams.append("labels", label));
     openInNewTab(url.toString());
 }

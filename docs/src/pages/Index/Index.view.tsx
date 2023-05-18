c16e042eff64c1742270af266e416aca55351152
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2023-05-18 06:43:01 +0200

    adding Home Page: layout and sections

---
+++ docs/src/pages/Index/Index.view.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HeroSection from '@site/src/components/Index/HeroSection';
+import FeaturesSection from '@site/src/components/Index/FeaturesSection';
+import QuickStartSection from '@site/src/components/Index/QuickStartSection';
+import ContactUsSection from '@site/src/components/Index/ContactUsSection';
+
+
+export default function Home(): React.JSX.Element {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description={`${siteConfig.tagline}`}>
+            <HeroSection/>
+            <main>
+                <FeaturesSection/>
+                <QuickStartSection/>
+                <ContactUsSection/>
+            </main>
+        </Layout>
+    );
+}

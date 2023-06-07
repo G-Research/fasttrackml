c1d3791f010dc8c0d6aab84b0b292331eaf242a8
Geoffrey Wilson <geoff@gr-oss.io>
2023-06-07 12:05:30 -0400

    Move runs to deleted stage when containing experiment is deleted (#28)
    * Move runs to deleted stage when containing experiment is deleted
    
    * Refactor to new repository/service pattern
    
    * Remove the separate service function for DeleteRunsByExperiment
    
    * Move archive-run to experiement repo with transaction
    
    * transaction simplification
    
    * remove tx.Rollback()

--- pkg/api/mlflow/dao/repositories/experiment.go
+++ pkg/api/mlflow/dao/repositories/experiment.go
@@ -79,8 +79,29 @@ func (r ExperimentRepository) GetByName(ctx context.Context, name string) (*mode
 
 // Update updates existing models.Experiment entity.
 func (r ExperimentRepository) Update(ctx context.Context, experiment *models.Experiment) error {
-	if err := r.db.WithContext(ctx).Model(&experiment).Updates(experiment).Error; err != nil {
-		return eris.Wrapf(err, "error updating experiment with id: %d", *experiment.ID)
+
+	if err := r.db.Transaction(func(tx *gorm.DB) error{
+
+		if err := r.db.WithContext(ctx).Model(&experiment).Updates(experiment).Error; err != nil {
+			return eris.Wrapf(err, "error updating experiment with id: %d", *experiment.ID)
+		}
+
+		// also archive experiment runs if experiment is being archived
+		if experiment.LifecycleStage == models.LifecycleStageDeleted {
+			run := models.Run{
+				LifecycleStage: experiment.LifecycleStage,
+				DeletedTime: experiment.LastUpdateTime,
+			}
+
+			if err := r.db.WithContext(ctx).Model(&run).Where("experiment_id = ?", experiment.ID).Updates(&run).Error; err != nil {
+				return eris.Wrapf(err, "error updating existing runs with experiment id: %d", *experiment.ID)
+			}
+		}
+		return nil
+
+	}); err != nil {
+		return err
 	}
+
 	return nil
 }

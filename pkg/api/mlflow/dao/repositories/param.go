e4bab19b98ad0f356eb87c131f8625f2325eb6d7
Geoff Wilson <geoff@gr-oss.io>
2023-06-08 18:15:58 -0400

    Added a method to remove any existing key/value/run_uuid matches from insert param batch

--- pkg/api/mlflow/dao/repositories/param.go
+++ pkg/api/mlflow/dao/repositories/param.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/rotisserie/eris"
 	"gorm.io/gorm"
@@ -31,8 +32,44 @@ func NewParamRepository(db *gorm.DB) *ParamRepository {
 
 // CreateBatch creates []models.Param entities in batch.
 func (r ParamRepository) CreateBatch(ctx context.Context, batchSize int, params []models.Param) error {
+	// skip inserting params which exactly match existing rows
+	params, err := r.removeExactMatches(ctx, params)
+	if err != nil {
+		return err
+	}
 	if err := r.db.CreateInBatches(params, batchSize).Error; err != nil {
 		return eris.Wrap(err, "error creating params in batch")
 	}
 	return nil
+}
+
+// removeExactMatches will return a new slice of params which excludes exact matches
+func (r ParamRepository) removeExactMatches(ctx context.Context, params []models.Param) ([]models.Param, error) {
+	var keys, foundKeys []string
+	paramMap := map[string]models.Param{}
+	paramsToReturn := []models.Param{}
+	for _, param := range params {
+		key := fmt.Sprintf("%v-%v-%v", param.RunID, param.Key, param.Value)
+		keys = append(keys, key)
+		paramMap[key] = param
+	}
+
+	tx := r.db.Raw(`
+           select run_uuid || '-' || key || '-' || value
+           from params
+           where run_uuid || '-' || key || '-' || value in ?`, keys).
+		Find(&foundKeys)
+	if tx.Error != nil {
+		return paramsToReturn, eris.Wrap(tx.Error, "problem selecting existing params")
+	}
+
+	for _, foundKey := range foundKeys {
+		delete(paramMap, foundKey)
+	}
+
+	for _, v := range paramMap {
+		paramsToReturn = append(paramsToReturn, v)
+	}
+
+	return paramsToReturn, nil
 }

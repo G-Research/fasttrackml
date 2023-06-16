8463dd645ad3e86d3e05bac2bcb592ff9cc644be
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2023-06-16 21:03:19 +0200

    Adding documentation website (#24)
    * adding .DS_Store to .gitignore
    
    * refactoring docs/ folder
    
    * adding .gitignore to docs/content/example for data/
    
    * bootstrapping Docusaurus project
    
    * adding static files
    
    * adding Home Page: layout and sections
    
    * adding CSS globals
    
    * adding Hero Section
    
    * adding Features Section
    
    * adding Quickstart Section
    
    * reverting changes on quickstart.md
    
    * fix spacing between sections
    
    * adding global styles to label, input and textarea to match infima.dev Design System
    
    * fix hero section links
    
    * adding ContactUs section
    
    * adding Docusaurus config
    
    * adding deploy website workflow
    
    * temporary fix GitHub Actions
    
    * deploy from Docs branch
    
    * deploy from main branch only
    
    * adding comment + using array of assignees
    
    * Changes:
    - Highlights
    - Footer
    - Page Title
    - Copyright
    - Hero Button
    
    * Updating QuickStart section
    
    * fix typo
    
    * fix typo
    
    * updating Landing Page content
    
    * fix broken links on README.md
    
    * cleaning
    
    * Improving deployment workflow
    
    * removing default assignees and labels from openNewIssue
    
    * replacing links: naskio with G-Research
    
    * updating copyright
    
    * removing Detect package manager from workflow and use Yarn
    
    * using new logo + icon
    
    * theming using new logo colors
    
    * updating URL to fasttrackml.io
    
    * adding comment: docs config outside project
    
    * renaming docs/ to website/
    
    * fix quickstart-section.md
    
    * reverting docs/ folder
    
    * reverting README.md
    
    * updating deploy-website.yml to work with website/ instead of docs/
    
    * fix link
    
    * fix docusaurus.config.js
    
    * using old image versions in quickstart-section.md
    
    * adding dependabot config for website/
    
    ---------
    
    Co-authored-by: Geoffrey Wilson <geoff@gr-oss.io>

---
+++ website/src/core/utils/index.ts
@@ -0,0 +1,22 @@
+const openInNewTab = (url: string) => {
+    window.open(url, '_blank', 'noreferrer');
+};
+
+export const openNewIssue = (base_url: string, title: string = "", body: string = "", assignees: string[] = [], labels: string[] = []) => {
+    /**
+     * https://docs.github.com/en/github-ae@latest/issues/tracking-your-work-with-issues/creating-an-issue
+     */
+    const url = new URL(base_url);
+    url.searchParams.append("title", title);
+    url.searchParams.append("body", body);
+    assignees.forEach(assignee => url.searchParams.append("assignees", assignee));
+    labels.forEach(label => url.searchParams.append("labels", label));
+    openInNewTab(url.toString());
+}
+
+export const openNewEmail = (email: string, subject: string = "", body: string = "") => {
+    const url = new URL(`mailto:${email}`);
+    url.searchParams.append("subject", subject);
+    url.searchParams.append("body", body);
+    openInNewTab(url.toString());
+}

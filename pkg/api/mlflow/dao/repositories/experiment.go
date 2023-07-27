29792890560d849a2e2c08af8970a34c04cbdd1b
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-07-26 19:59:07 -1000

    Fix deadlock during experiment update

--- pkg/api/mlflow/dao/repositories/experiment.go
+++ pkg/api/mlflow/dao/repositories/experiment.go
@@ -88,7 +88,7 @@ func (r ExperimentRepository) GetByName(ctx context.Context, name string) (*mode
 // Update updates existing models.Experiment entity.
 func (r ExperimentRepository) Update(ctx context.Context, experiment *models.Experiment) error {
 	if err := r.db.Transaction(func(tx *gorm.DB) error {
-		if err := r.db.WithContext(ctx).Model(&experiment).Updates(experiment).Error; err != nil {
+		if err := tx.WithContext(ctx).Model(&experiment).Updates(experiment).Error; err != nil {
 			return eris.Wrapf(err, "error updating experiment with id: %d", *experiment.ID)
 		}
 
@@ -99,7 +99,7 @@ func (r ExperimentRepository) Update(ctx context.Context, experiment *models.Exp
 				DeletedTime:    experiment.LastUpdateTime,
 			}
 
-			if err := r.db.WithContext(ctx).Model(&run).Where("experiment_id = ?", experiment.ID).Updates(&run).Error; err != nil {
+			if err := tx.WithContext(ctx).Model(&run).Where("experiment_id = ?", experiment.ID).Updates(&run).Error; err != nil {
 				return eris.Wrapf(err, "error updating existing runs with experiment id: %d", *experiment.ID)
 			}
 		}

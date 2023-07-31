4adbf229c134d262f346cfec77c756ea011764cb
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-07-31 12:29:22 +0000

    run

--- pkg/api/aim/request/run.go
+++ pkg/api/aim/request/run.go
@@ -1,7 +1,7 @@
 // TODO move this beneath package 'api' when available
 package request
 
-// UpdateRun represents the data to archive or update a Run
+// UpdateRun represents the  data to archive or update a Run
 type UpdateRun struct {
 	Name        *string `json:"name"`
 	Description *string `json:"description"`

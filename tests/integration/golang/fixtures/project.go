c70dd6e4a99cba9d96678571601b28831fb7824a
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-08-01 05:00:41 +0100

    Add fixtures to get project activity

--- tests/integration/golang/fixtures/project.go
+++ tests/integration/golang/fixtures/project.go
@@ -6,8 +6,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rotisserie/eris"
-
-	// "github.com/G-Research/fasttrackml/pkg/api/mlflow/dao/models"
+ 
 	"github.com/G-Research/fasttrackml/pkg/database"
 )
 
@@ -41,4 +40,50 @@ func (f *ProjectFixtures) GetProject(ctx context.Context) *fiber.Map {
 		"description":       "",
 		"telemetry_enabled": float64(0),
 	}
+}
+
+func (f *ProjectFixtures) GetProjectActivity(
+	ctx context.Context,
+) (*fiber.Map, error) {
+	var numExperiments int64
+	if err := f.db.WithContext(ctx).
+		Table("experiments").
+		Count(&numExperiments).Error; err != nil {
+		return nil, eris.Wrapf(err, "error counting experiments")
+	}
+
+	var numRuns int64
+	if err := f.db.WithContext(ctx).
+		Table("runs").
+		Count(&numRuns).Error; err != nil {
+		return nil, eris.Wrapf(err, "error counting runs")
+	}
+
+	var numActiveRuns int64
+	if err := f.db.WithContext(ctx).
+		Table("runs").
+		Where("status = ?", database.StatusRunning).
+		Count(&numActiveRuns).Error; err != nil {
+		return nil, eris.Wrapf(err, "error counting active runs")
+	}
+
+	var numArchivedRuns int64
+	if err := f.db.WithContext(ctx).
+		Table("runs").
+		Where("lifecycle_stage = ?", database.LifecycleStageDeleted).
+		Count(&numArchivedRuns).Error; err != nil {
+		return nil, eris.Wrapf(err, "error counting archived runs")
+	}
+
+    activity := map[string]int{}
+    key := time.Now().Format("2006-01-02T15:00:00")
+	activity[key] = int(numRuns)
+
+	return &fiber.Map{
+		"num_experiments":   float64(numExperiments),
+		"num_runs":          float64(numRuns),
+		"num_archived_runs": float64(numArchivedRuns),
+		"num_active_runs":   float64(numActiveRuns),
+		"activity_map":      activity,
+	}, nil
 }

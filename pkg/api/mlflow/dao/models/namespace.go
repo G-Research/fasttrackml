f96d9b2f070343801472f18b16512f6e18291965
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-08-01 17:42:38 -0700

    schema

---
+++ pkg/api/mlflow/dao/models/namespace.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Namespace represents model to work with `namespaces` table.
+type Namespace struct {
+	ID          uint   `gorm:"primaryKey;autoIncrement"`
+	Code        string `gorm:"unique;index;not null"`
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Experiments []Experiment   `gorm:"constraint:OnDelete:CASCADE"`
+}

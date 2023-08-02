7059cc8a85ce1c543e31c0c0a7dc7c47c196d162
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-08-01 17:42:38 -0700

    schema

--- pkg/api/mlflow/dao/models/common.go
+++ pkg/api/mlflow/dao/models/common.go
@@ -1,17 +1,5 @@
 package models
 
-// Status represents Status type which is using in different models.
-type Status string
-
-// Supported list of statuses.
-const (
-	StatusRunning   Status = "RUNNING"
-	StatusScheduled Status = "SCHEDULED"
-	StatusFinished  Status = "FINISHED"
-	StatusFailed    Status = "FAILED"
-	StatusKilled    Status = "KILLED"
-)
-
 // LifecycleStage represents entity stage
 type LifecycleStage string
 
--- pkg/database/model.go
+++ pkg/database/model.go
@@ -30,13 +30,25 @@ const (
 	LifecycleStageDeleted LifecycleStage = "deleted"
 )
 
+type Namespace struct {
+	ID          uint   `gorm:"primaryKey;autoIncrement"`
+	Code        string `gorm:"unique;index;not null"`
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Experiments []Experiment   `gorm:"constraint:OnDelete:CASCADE"`
+}
+
 type Experiment struct {
-	ID               *int32          `gorm:"column:experiment_id;not null;primaryKey"`
-	Name             string          `gorm:"type:varchar(256);not null;unique"`
-	ArtifactLocation string          `gorm:"type:varchar(256)"`
-	LifecycleStage   LifecycleStage  `gorm:"type:varchar(32);check:lifecycle_stage IN ('active', 'deleted')"`
-	CreationTime     sql.NullInt64   `gorm:"type:bigint"`
-	LastUpdateTime   sql.NullInt64   `gorm:"type:bigint"`
+	ID               *int32         `gorm:"column:experiment_id;not null;primaryKey"`
+	Name             string         `gorm:"type:varchar(256);not null;unique"`
+	ArtifactLocation string         `gorm:"type:varchar(256)"`
+	LifecycleStage   LifecycleStage `gorm:"type:varchar(32);check:lifecycle_stage IN ('active', 'deleted')"`
+	CreationTime     sql.NullInt64  `gorm:"type:bigint"`
+	LastUpdateTime   sql.NullInt64  `gorm:"type:bigint"`
+	NamespaceID      uint
+	Namespace        Namespace
 	Tags             []ExperimentTag `gorm:"constraint:OnDelete:CASCADE"`
 	Runs             []Run           `gorm:"constraint:OnDelete:CASCADE"`
 }

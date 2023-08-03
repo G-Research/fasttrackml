7c830b0460b8a65f6cbfc1d355e8d9ef934039fc
dsuhinin <suhinin.dmitriy@gmail.com>
2023-08-03 23:49:53 +0200

    [FML-199] - in progress.

--- tests/integration/golang/mlflow/metric/get_histories_test.go
+++ tests/integration/golang/mlflow/metric/get_histories_test.go
@@ -1,3 +1,42 @@
 //go:build integration
 
 package metric
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/G-Research/fasttrackml/tests/integration/golang/fixtures"
+	"github.com/G-Research/fasttrackml/tests/integration/golang/helpers"
+)
+
+type GetHistoriesTestSuite struct {
+	suite.Suite
+	client             *helpers.HttpClient
+	runFixtures        *fixtures.RunFixtures
+	metricFixtures     *fixtures.MetricFixtures
+	experimentFixtures *fixtures.ExperimentFixtures
+}
+
+func TestGetHistoriesTestSuite(t *testing.T) {
+	suite.Run(t, new(GetHistoriesTestSuite))
+}
+
+func (s *GetHistoriesTestSuite) SetupTest() {
+	s.client = helpers.NewMlflowApiClient(helpers.GetServiceUri())
+	runFixtures, err := fixtures.NewRunFixtures(helpers.GetDatabaseUri())
+	assert.Nil(s.T(), err)
+	s.runFixtures = runFixtures
+	metricFixtures, err := fixtures.NewMetricFixtures(helpers.GetDatabaseUri())
+	assert.Nil(s.T(), err)
+	s.metricFixtures = metricFixtures
+	experimentFixtures, err := fixtures.NewExperimentFixtures(helpers.GetDatabaseUri())
+	assert.Nil(s.T(), err)
+	s.experimentFixtures = experimentFixtures
+}
+
+func (s *GetHistoriesTestSuite) Test_Ok() {}
+
+func (s *GetHistoriesTestSuite) Test_Error() {}

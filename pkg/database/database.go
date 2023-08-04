f96e99b29583b96b6c202e33a13fd8363137d2c7
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-08-03 20:53:05 -0700

    schema

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -489,6 +489,12 @@ func checkAndMigrateDB(db *DbInstance, migrate bool) error {
 						if err := tx.Migrator().CreateConstraint(&Namespace{}, "Experiments"); err != nil {
 							return err
 						}
+						if err := tx.Migrator().AlterColumn(&Experiment{}, "Name"); err != nil {
+							return err
+						}
+						if err := tx.Migrator().CreateIndex(&Experiment{}, "idx_namespace_name"); err != nil {
+							return err
+						}
 						return tx.Model(&SchemaVersion{}).
 							Where("1 = 1").
 							Update("Version", "e0d125c68d9a").

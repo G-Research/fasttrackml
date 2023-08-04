d237607b9d6166c575279d5353d3813b353d47bd
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-08-03 20:52:55 -0700

    middleware

--- pkg/api/middleware/namespace/namespace.go
+++ pkg/api/middleware/namespace/namespace.go
@@ -8,6 +8,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+
+	"github.com/G-Research/fasttrackml/pkg/database"
 )
 
 const (
@@ -31,14 +34,35 @@ func New() fiber.Handler {
 	}
 }
 
-func GetFromFiber(c *fiber.Ctx) string {
-	return GetFromContext(c.Context())
-}
-
-func GetFromContext(c context.Context) string {
+func GetCodeFromContext(c context.Context) string {
 	ns, ok := c.Value(key).(string)
 	if !ok {
 		return defaultValue
 	}
 	return ns
+}
+
+func GetCodeFromFiber(c *fiber.Ctx) string {
+	return GetCodeFromContext(c.Context())
+}
+
+func GetIDFromCode(db *gorm.DB, code string) (uint, error) {
+	var ns database.Namespace
+	if err := db.
+		Select("ID").
+		Where("code = ?", code).
+		First(&ns).
+		Error; err != nil {
+		return 0, err
+	}
+	return ns.ID, nil
+}
+
+func GetIDFromContext(db *gorm.DB, c context.Context) (uint, error) {
+	code := GetCodeFromContext(c)
+	return GetIDFromCode(db, code)
+}
+
+func GetIDFromFiber(db *gorm.DB, c *fiber.Ctx) (uint, error) {
+	return GetIDFromContext(db, c.Context())
 }

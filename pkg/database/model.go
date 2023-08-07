13fd6c0f42feb236a2877e8a1d185c427bb6c510
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-08-07 15:50:22 +0100

    add default experiment ID to namespace schema

--- pkg/database/model.go
+++ pkg/database/model.go
@@ -31,13 +31,14 @@ const (
 )
 
 type Namespace struct {
-	ID          uint   `gorm:"primaryKey;autoIncrement"`
-	Code        string `gorm:"unique;index;not null"`
-	Description string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
-	Experiments []Experiment   `gorm:"constraint:OnDelete:CASCADE"`
+	ID                  uint   `gorm:"primaryKey;autoIncrement"`
+	Code                string `gorm:"unique;index;not null"`
+	Description         string
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	DeletedAt           gorm.DeletedAt `gorm:"index"`
+	DefaultExperimentID *int32         `gorm:"not null"`
+	Experiments         []Experiment   `gorm:"constraint:OnDelete:CASCADE"`
 }
 
 type Experiment struct {

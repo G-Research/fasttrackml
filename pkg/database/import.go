e10345cc0e912fad9377fdc8886f3a814c1365de
Geoff Wilson <geoff@gr-oss.io>
2023-08-08 17:42:38 -0400

    Trust primary key / unique constraints to prevent collisions

--- pkg/database/import.go
+++ pkg/database/import.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rotisserie/eris"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 // Import will copy the contents of input db to output db.
@@ -49,118 +50,23 @@ func Import(input, output *DbInstance, dryRun bool) error {
 // importTable will copy the contents of one table (model) from sourceDB to destDB.
 func importTable[T any](sourceDB, destDB *gorm.DB, dryRun bool, model T) error {
 	// Query data from the source database
-	log.Infof("Importing %T", model)
 	rows, err := sourceDB.Model(&model).Rows()
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
 
-	log.Infof("Transferring records (dry run? %v)", dryRun)
-
-	// Stream data to the destination database in bulk with collision handling
+	count := 0
 	for rows.Next() {
 		var item T
 		sourceDB.ScanRows(rows, &item)
-		found, err := findCollision(destDB, item)
-		if err != nil {
-			return err
-		}
-		if found {
-			// Handle the collision with a warning
-			log.Infof(
-				`Skipping record (type/values) '%T/%v', already present in dest`,
-				item,
-				item,
-			)
-		} else {
-			if !dryRun {
-				destDB.Create(&item)
+		if !dryRun {
+			if err := destDB.Clauses(clause.OnConflict{DoNothing: true}).Create(&item).Error; err != nil {
+				return err
 			}
 		}
+		count++
 	}
+	log.Infof("Importing %T - found %v records", model, count)
 	return nil
-}
-
-// findCollision will return true when the sourceItem appears to already exist in the
-// destination DB
-func findCollision(destDB *gorm.DB, sourceItem any) (bool, error) {
-	switch typedItem := sourceItem.(type) {
-	case Experiment:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"name = ?",
-			typedItem.Name,
-		).Count(&c)
-		return c > 0, tx.Error
-	case ExperimentTag:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"experiment_id = ? AND key = ?",
-			typedItem.ExperimentID,
-			typedItem.Key,
-		).Count(&c)
-		return c > 0, tx.Error
-	case Run:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"run_uuid = ?",
-			typedItem.ID,
-		).Count(&c)
-		return c > 0, tx.Error
-	case Param:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"run_uuid = ? AND key = ?",
-			typedItem.RunID,
-			typedItem.Key,
-		).Count(&c)
-		return c > 0, tx.Error
-	case Tag:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"run_uuid = ? AND key = ?",
-			typedItem.RunID,
-			typedItem.Key,
-		).Count(&c)
-		return c > 0, tx.Error
-	case Metric:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"run_uuid = ? AND key = ? AND value = ? AND timestamp = ? AND step = ? AND is_nan = ?",
-			typedItem.RunID,
-			typedItem.Key,
-			typedItem.Value,
-			typedItem.Timestamp,
-			typedItem.Step,
-			typedItem.IsNan,
-		).Count(&c)
-		return c > 0, tx.Error
-	case LatestMetric:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"run_uuid = ? AND key = ? AND value = ? AND timestamp = ? AND step = ? AND is_nan = ?",
-			typedItem.RunID,
-			typedItem.Key,
-			typedItem.Value,
-			typedItem.Timestamp,
-			typedItem.Step,
-			typedItem.IsNan,
-		).Count(&c)
-		return c > 0, tx.Error
-	case Dashboard:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where(
-			"name = ? AND app_id = ?",
-			typedItem.Name,
-			typedItem.AppID,
-		).Count(&c)
-		return c > 0, tx.Error
-	case App:
-		c := int64(0)
-		tx := destDB.Model(typedItem).Where("id = ? ", typedItem.ID).Count(&c)
-		return c > 0, tx.Error
-	default:
-		return false, eris.New("Could not determine source item type")
-	}
 }

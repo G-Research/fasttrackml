6b11bcfc923b010e487c93add5202ef065971305
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-08-14 11:41:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,40 @@
 import os
 import platform
+import re
 from setuptools.command.sdist import sdist
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
-from wheel.bdist_wheel import bdist_wheel
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 import subprocess
-import logging
+import tarfile
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open("README.md", "r") as f:
     readme = f.read()
 
+def get_version():
+    version_string=""
+    if platform.system() == "Linux":
+        my_tar = tarfile.open('./dist/fasttrackml_linux_x86_64.tar.gz')
+        my_tar.extractall('./dist/fasttrackml_linux_x86_64') # specify which folder to extract to
+        my_tar.close()
+        version_string = subprocess.check_output(
+            [f"./dist/fasttrackml_linux_x86_64/fml", "--version"], universal_newlines=True
+        ).strip()
+    else:
+        my_tar = tarfile.open('./dist/fasttrackml_macos_x86_64.tar.gz')
+        my_tar.extractall('./dist/fasttrackml_macos_x86_64') # specify which folder to extract to
+        my_tar.close()
+        version_string = subprocess.check_output(
+            [f"./dist/fasttrackml_macos_x86_64/fml", "--version"], universal_newlines=True
+        ).strip()
+    matches = re.findall(r'\d+\.\d+\.\d+', version_string)
+    for match in matches:
+        return match
+
 def get_fml_executable():
-    system = platform.system()
-    return "bin/fml.exe" if system == "Windows" else "bin/fml"
+     for file_name in os.listdir("bin"):
+         if "fml" in file_name:
+             return os.path.join("bin", file_name)
 
 class FmlExtension(Extension):
     """Extension for `fml`"""
@@ -21,17 +43,23 @@ class FmlBuildExt(build_ext):
     def build_extension(self, ext: Extension) -> None:
         if not isinstance(ext, FmlExtension):
             return super().build_extension(ext)
-def get_version():
-    # Remove prefix v in versioning
-    version = subprocess.check_output(
-        [f'./bin/{get_fml_executable()}', "version", "--short"], universal_newlines=True
-    ).strip()
-    ver = version.rsplit(" ", 1)[-1][1:]
-    return ver
+
+class bdist_wheel(_bdist_wheel):
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't contain any python source
+            python, abi = 'py3', 'none'
+            return python, abi, plat
 
 setup(
     name="fml",
-    version="1.0.0",
+    version=get_version(),
     description="Rewrite of the MLFlow tracking server with a focus on scalability.",
     long_description=readme,
     packages=find_packages(),
@@ -40,11 +68,10 @@ setup(
     python_requires=">=3.6",
     zip_safe=False,
     ext_modules=[
-         FmlExtension(name="fml", sources=["cmd/*"]),
-     ],
+        FmlExtension(name="fml", sources=["cmd/*"]),
+    ],
     cmdclass=dict(
-         build_ext=FmlBuildExt,
-         sdist=sdist,
-         bdist_wheel=bdist_wheel,
-     ),
+        bdist_wheel=bdist_wheel,
+        build_ext=FmlBuildExt,
+    ),
 )

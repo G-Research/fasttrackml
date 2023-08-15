932fc428e251cba960b57a47f94a68253aa529e1
Geoff Wilson <geoff@gr-oss.io>
2023-08-15 15:39:43 -0400

    Test no-data GetActiveRuns better

--- tests/integration/golang/aim/run/get_runs_active_test.go
+++ tests/integration/golang/aim/run/get_runs_active_test.go
@@ -41,34 +41,36 @@ func (s *GetRunsActiveTestSuite) SetupTest() {
 	expFixtures, err := fixtures.NewExperimentFixtures(helpers.GetDatabaseUri())
 	assert.Nil(s.T(), err)
 	s.experimentFixtures = expFixtures
-
-	exp := &models.Experiment{
-		Name:           uuid.New().String(),
-		LifecycleStage: models.LifecycleStageActive,
-	}
-	_, err = s.experimentFixtures.CreateExperiment(context.Background(), exp)
-	assert.Nil(s.T(), err)
-
-	s.runs, err = s.runFixtures.CreateExampleRuns(context.Background(), exp, 3)
-	assert.Nil(s.T(), err)
-
-	// set 3rd run to status = StatusFinished
-	s.runs[2].Status = models.StatusFinished
-	assert.Nil(s.T(), s.runFixtures.UpdateRun(context.Background(), s.runs[2]))
 }
 
 func (s *GetRunsActiveTestSuite) Test_Ok() {
 	defer func() {
 		assert.Nil(s.T(), s.runFixtures.UnloadFixtures())
 	}()
+
 	tests := []struct {
 		name         string
 		wantRunCount int
 		beforeRunFn  func()
 	}{
 		{
+			name:         "GetActiveRunsWithNoData",
+			wantRunCount: 0,
+		},
+		{
 			name:         "GetActiveRuns",
 			wantRunCount: 3,
+			beforeRunFn: func() {
+				exp := &models.Experiment{
+					Name:           uuid.New().String(),
+					LifecycleStage: models.LifecycleStageActive,
+				}
+				_, err := s.experimentFixtures.CreateExperiment(context.Background(), exp)
+				assert.Nil(s.T(), err)
+
+				s.runs, err = s.runFixtures.CreateExampleRuns(context.Background(), exp, 3)
+				assert.Nil(s.T(), err)
+			},
 		},
 		{
 			name:         "GetActiveRunsSkipsFinished",
@@ -77,17 +79,6 @@ func (s *GetRunsActiveTestSuite) Test_Ok() {
 				// set 3rd run to status = StatusFinished
 				s.runs[2].Status = models.StatusFinished
 				assert.Nil(s.T(), s.runFixtures.UpdateRun(context.Background(), s.runs[2]))
-			},
-		},
-		{
-			name:         "GetActiveRunsWhenEmpty",
-			wantRunCount: 0,
-			beforeRunFn: func() {
-				runIDs := make([]string, len(s.runs))
-				for idx, run := range s.runs {
-					runIDs[idx] = run.ID
-				}
-				assert.Nil(s.T(), s.runFixtures.ArchiveRun(context.Background(), runIDs))
 			},
 		},
 	}
@@ -106,20 +97,22 @@ func (s *GetRunsActiveTestSuite) Test_Ok() {
 			decodedData, err := encoding.Decode(bytes.NewBuffer(data))
 			assert.Nil(s.T(), err)
 
-			for idx := 0; idx < tt.wantRunCount; idx++ {
-				respNameKey := fmt.Sprintf("%v.props.name", s.runs[idx].ID)
-				expIdKey := fmt.Sprintf("%v.props.experiment.id", s.runs[idx].ID)
-				startTimeKey := fmt.Sprintf("%v.props.creation_time", s.runs[idx].ID)
-				endTimeKey := fmt.Sprintf("%v.props.end_time", s.runs[idx].ID)
-				activeKey := fmt.Sprintf("%v.props.active", s.runs[idx].ID)
-				archivedKey := fmt.Sprintf("%v.props.archived", s.runs[idx].ID)
-				if s.runs[idx].Status == models.StatusRunning {
-					assert.Equal(s.T(), s.runs[idx].Name, decodedData[respNameKey])
-					assert.Equal(s.T(), fmt.Sprintf("%v", s.runs[idx].ExperimentID), decodedData[expIdKey])
-					assert.Equal(s.T(), s.runs[idx].Status == models.StatusRunning, decodedData[activeKey])
-					assert.Equal(s.T(), s.runs[idx].LifecycleStage == models.LifecycleStageDeleted, decodedData[archivedKey])
-					assert.Equal(s.T(), s.runs[idx].StartTime.Int64, int64(decodedData[startTimeKey].(float64)))
-					assert.Equal(s.T(), s.runs[idx].EndTime.Int64, int64(decodedData[endTimeKey].(float64)))
+			responseCount := 0
+			for _, run := range s.runs {
+				respNameKey := fmt.Sprintf("%v.props.name", run.ID)
+				expIdKey := fmt.Sprintf("%v.props.experiment.id", run.ID)
+				startTimeKey := fmt.Sprintf("%v.props.creation_time", run.ID)
+				endTimeKey := fmt.Sprintf("%v.props.end_time", run.ID)
+				activeKey := fmt.Sprintf("%v.props.active", run.ID)
+				archivedKey := fmt.Sprintf("%v.props.archived", run.ID)
+				if run.Status == models.StatusRunning && run.LifecycleStage == models.LifecycleStageActive {
+					assert.Equal(s.T(), run.Name, decodedData[respNameKey])
+					assert.Equal(s.T(), fmt.Sprintf("%v", run.ExperimentID), decodedData[expIdKey])
+					assert.Equal(s.T(), run.Status == models.StatusRunning, decodedData[activeKey])
+					assert.Equal(s.T(), false, decodedData[archivedKey])
+					assert.Equal(s.T(), run.StartTime.Int64, int64(decodedData[startTimeKey].(float64)))
+					assert.Equal(s.T(), run.EndTime.Int64, int64(decodedData[endTimeKey].(float64)))
+					responseCount++
 				} else {
 					assert.Nil(s.T(), decodedData[respNameKey])
 					assert.Nil(s.T(), decodedData[expIdKey])
@@ -129,6 +122,7 @@ func (s *GetRunsActiveTestSuite) Test_Ok() {
 					assert.Nil(s.T(), decodedData[endTimeKey])
 				}
 			}
+			assert.Equal(s.T(), tt.wantRunCount, responseCount)
 		})
 	}
 }

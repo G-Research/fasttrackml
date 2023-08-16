88fc98e390cd9e744f6c367dad92004f2c1a24c2
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-08-16 11:05:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,52 +1,52 @@
 import os
-import platform
+import subprocess
 import re
 from setuptools.command.sdist import sdist
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-import subprocess
-import tarfile
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
-def get_version():
-    path = './dist/fasttrackml_linux_x86_64' if platform.system() == "Linux" else './dist/fasttrackml_macos_x86_64'
-    bin = tarfile.open(f'{path}.tar.gz')
-    bin.extractall(path)
-    bin.close()
-    version_string = subprocess.check_output(
-        [f"{path}/fml", "--version"], universal_newlines=True
-    ).strip()
-    matches = re.findall(r'\d+\.\d+\.\d+', version_string)
-    return matches[0]
 
 def get_fml_executable():
-     for file_name in os.listdir("bin"):
-         if "fml" in file_name:
-             return os.path.join("bin", file_name)
+    os = subprocess.check_output(["go", "env", "GOOS"]).strip().decode("utf-8")
+    return "fml.exe" if os == "windows" else "fml"
 
-class FmlExtension(Extension):
-    """Extension for `fml`"""
+def get_version():
+    version = os.environ.get("VERSION")
+    matches = re.findall(r'\d+\.\d+\.\d+', version)
+    return matches[0]
 
-class FmlBuildExt(build_ext):
-    def build_extension(self, ext: Extension) -> None:
-        if not isinstance(ext, FmlExtension):
-            return super().build_extension(ext)
+def get_platform():
+    os = subprocess.check_output(["go", "env", "GOOS"]).strip().decode("utf-8")
+    arch = subprocess.check_output(["go", "env", "GOARCH"]).strip().decode("utf-8")
+    print(f"{os}_{arch}")
+    if f"{os}_{arch}" == "darwin_amd64":
+        return "macosx_10_13_x86_64"
+    elif f"{os}_{arch}" == "darwin_arm64":
+        return "macosx_11_0_arm64"
+    elif f"{os}_{arch}" == "linux_amd64":
+        return "manylinux1_x86_64"
+    elif f"{os}_{arch}" == "linux_arm64":
+        return "manylinux1_aarch64"
+    elif f"{os}_{arch}" == "windows_amd64":
+        return "win_amd64"
 
 class bdist_wheel(_bdist_wheel):
 
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            # Mark us as not a pure python package
-            self.root_is_pure = False
+    def finalize_options(self):
+        _bdist_wheel.finalize_options(self)
+        # Mark us as not a pure python package
+        self.root_is_pure = False
 
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            # We don't contain any python source
-            python, abi = 'py3', 'none'
-            return python, abi, plat
+    def get_tag(self):
+        python, abi, plat = _bdist_wheel.get_tag(self)
+        # We don't contain any python source
+        python, abi = 'py3', 'none'
+        return 'py3', 'none', get_platform()
+
 
 setup(
     name="fml",
@@ -58,11 +58,9 @@ setup(
     data_files=[("bin", [get_fml_executable()])],
     python_requires=">=3.6",
     zip_safe=False,
-    ext_modules=[
-        FmlExtension(name="fml", sources=["cmd/*"]),
-    ],
+    ext_modules=[],
     cmdclass=dict(
+        sdist=sdist,
         bdist_wheel=bdist_wheel,
-        build_ext=FmlBuildExt,
     ),
 )

ebd6f115787a3569ff1ee49e8bbef49965277202
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-08-21 16:05:34 +0100

    Add cases for (-1,0,1)

--- pkg/api/mlflow/dao/repositories/run_test.go
+++ pkg/api/mlflow/dao/repositories/run_test.go
@@ -11,22 +11,52 @@ import (
 	"github.com/G-Research/fasttrackml/pkg/api/mlflow/dao/models"
 )
 
-func TestRenumberRowsForNegativeRowNumber(t *testing.T) {
-	mockDb, _, err := sqlmock.New()
+func Test_renumberRows(t *testing.T) {
+	testData := []struct {
+		name      string
+		startWith models.RowNum
+	}{
+		{
+			name:      "NegativeRowNumber",
+			startWith: models.RowNum(-1),
+		},
+		{
+			name:      "ZeroRowNumber",
+			startWith: models.RowNum(0),
+		},
+		{
+			name:      "PositiveRowNumber",
+			startWith: models.RowNum(1),
+		},
+	}
+
+	mockDb, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer mockDb.Close()
+
+	lockExpect := func() {
+		mock.ExpectExec("LOCK TABLE runs").WillReturnResult(sqlmock.NewResult(0, 1))
+		mock.ExpectExec(`UPDATE runs`).WillReturnResult(sqlmock.NewResult(0, 1))
+	}
+
 	dialector := postgres.New(postgres.Config{
 		Conn:       mockDb,
 		DriverName: "postgres",
 	})
 	db, _ := gorm.Open(dialector, &gorm.Config{})
 
-	startWith := models.RowNum(-1)
-
 	repo := NewRunRepository(db)
-	err = repo.renumberRows(db, startWith)
 
-	assert.EqualError(t, err, "attempting to renumber with less than 0 row number value")
+	for _, tc := range testData {
+		if tc.startWith < 0 {
+			err = repo.renumberRows(db, tc.startWith)
+			assert.EqualError(t, err, "attempting to renumber with less than 0 row number value")
+		} else {
+			lockExpect()
+			err = repo.renumberRows(db, tc.startWith)
+			assert.NoError(t, err)
+		}
+	}
 }

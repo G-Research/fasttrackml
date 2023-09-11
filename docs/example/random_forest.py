c704d3c57c424b33b82ce7372f2add825e59b21a
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-09-11 16:06:03 +0100

    Fix quickstart

--- docs/example/random_forest.py
+++ docs/example/random_forest.py
@@ -8,7 +8,7 @@ ALL_FILES = ["data/0.csv", "data/1.csv", "data/2.csv", "data/3.csv"]
 mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment("my-experiment")
 
-mlflow.sklearn.autolog(log_models=False)
+mlflow.sklearn.autolog(log_models=False, log_datasets=False)
 
 
 def read_data():

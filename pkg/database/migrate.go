d477526204075b63a9772d1fe0236f466c6e0e3e
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-09-12 18:04:47 +0100

    Fix regression in compatibility with existing MLFlow databases
    This got introduced in #72 and #134.

--- pkg/database/migrate.go
+++ pkg/database/migrate.go
@@ -221,13 +221,28 @@ func checkAndMigrate(migrate bool, dbProvider DBProvider) error {
 				if err := db.Transaction(func(tx *gorm.DB) error {
 					constraints := []string{"Params", "Tags", "Metrics", "LatestMetrics"}
 					for _, constraint := range constraints {
-						// SQLite tables need to be recreated to add or remove constraints.
-						// By not dropping the constraint, we can avoid having to recreate the table twice.
-						if db.Dialector.Name() != "sqlite" {
-							if err := tx.Migrator().DropConstraint(&Run{}, constraint); err != nil {
-								return err
+						switch tx.Dialector.Name() {
+						case "sqlite":
+							// SQLite tables need to be recreated to add or remove constraints.
+							// By not dropping the constraint, we can avoid having to recreate the table twice.
+						case "postgres":
+							// Existing MLFlow Postgres databases have foreign key constraints
+							// with their own names. We need to drop them before we can add our own.
+							table := tx.NamingStrategy.TableName(constraint)
+							fk := fmt.Sprintf("%s_run_uuid_fkey", table)
+							if tx.Migrator().HasConstraint(table, fk) {
+								if err := tx.Migrator().DropConstraint(table, fk); err != nil {
+									return err
+								}
+							} else {
+								if err := tx.Migrator().DropConstraint(&Run{}, constraint); err != nil {
+									return err
+								}
 							}
+						default:
+							return fmt.Errorf("unsupported database dialect %s", tx.Dialector.Name())
 						}
+
 						if err := tx.Migrator().CreateConstraint(&Run{}, constraint); err != nil {
 							return err
 						}
@@ -247,16 +262,36 @@ func checkAndMigrate(migrate bool, dbProvider DBProvider) error {
 				// the cascading delete to kick in and delete all the run data.
 				if err := runWithoutForeignKeyIfNeeded(func() error {
 					if err := db.Transaction(func(tx *gorm.DB) error {
-						constraints := []string{"Tags", "Runs"}
-						for _, constraint := range constraints {
-							// SQLite tables need to be recreated to add or remove constraints.
-							// By not dropping the constraint, we can avoid having to recreate the table twice.
-							if db.Dialector.Name() != "sqlite" {
-								if err := tx.Migrator().DropConstraint(&Experiment{}, constraint); err != nil {
-									return err
+						elems := []struct {
+							Table      string
+							Constraint string
+						}{
+							{"experiment_tags", "Tags"},
+							{"runs", "Runs"},
+						}
+						for _, e := range elems {
+							switch tx.Dialector.Name() {
+							case "sqlite":
+								// SQLite tables need to be recreated to add or remove constraints.
+								// By not dropping the constraint, we can avoid having to recreate the table twice.
+							case "postgres":
+								// Existing MLFlow Postgres databases have foreign key constraints with their own names.
+								// We need to drop them before we can add our own.
+								fk := fmt.Sprintf("%s_experiment_id_fkey", e.Table)
+								if tx.Migrator().HasConstraint(e.Table, fk) {
+									if err := tx.Migrator().DropConstraint(e.Table, fk); err != nil {
+										return err
+									}
+								} else {
+									if err := tx.Migrator().DropConstraint(&Experiment{}, e.Constraint); err != nil {
+										return err
+									}
 								}
+							default:
+								return fmt.Errorf("unsupported database dialect %s", tx.Dialector.Name())
 							}
-							if err := tx.Migrator().CreateConstraint(&Experiment{}, constraint); err != nil {
+
+							if err := tx.Migrator().CreateConstraint(&Experiment{}, e.Constraint); err != nil {
 								return err
 							}
 						}

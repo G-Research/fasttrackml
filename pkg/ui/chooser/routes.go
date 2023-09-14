faff693391e074b5b54d750aa7bdaba6d5d9231f
Geoff Wilson <geoff@gr-oss.io>
2023-09-14 14:43:07 -0400

    Fix merge from dmitriy's branch

--- pkg/ui/chooser/routes.go
+++ pkg/ui/chooser/routes.go
@@ -5,6 +5,7 @@ import (
 	"io/fs"
 	"net/http"
 
+	"github.com/G-Research/fasttrackml/pkg/ui/common"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 
 	"github.com/gofiber/fiber/v2"
@@ -16,8 +17,13 @@ var content embed.FS
 
 func AddRoutes(r fiber.Router) {
 	sub, _ := fs.Sub(content, "embed")
+	r.Use("/static/chooser/", etag.New(), filesystem.New(filesystem.Config{
+		Root: http.FS(sub),
+	}))
 
 	r.Use("/", etag.New(), filesystem.New(filesystem.Config{
-		Root: http.FS(sub),
+		Root: http.FS(
+			common.NewOnlyRootFS(sub, "index.html"),
+		),
 	}))
 }

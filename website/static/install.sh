f4f3723f42cd8d2fdcddb8f4bce8bf086e78c3d0
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-09-14 15:14:23 +0100

    Fix install scripts (#313)

--- website/static/install.sh
+++ website/static/install.sh
@@ -2,18 +2,33 @@
 
 set -e
 
-if [ "$(uname -s)" = "Darwin" ] && [ "$(uname -m)" = "x86_64" ]; then
-    target="macos_x86_64"
-elif [ "$(uname -s)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]; then
-    target="macos_arm64"
-elif [ "$(uname -s)" = "Linux" ] && [ "$(uname -m)" = "x86_64" ]; then
-    target="linux_x86_64"
-elif [ "$(uname -s)" = "Linux" ] && [ "$(uname -m)" = "arm64" ]; then
-    target="linux_arm64"
-else
-    echo "Unsupported OS or architecture"
-    exit 1
-fi
+case $(uname -s) in
+    "Darwin")
+        reverse="tail -r"
+        os="macos"
+        ;;
+    "Linux")
+        reverse="tac"
+        os="linux"
+        ;;
+    *)
+        echo "Unsupported OS"
+        exit 1
+        ;;
+esac
+
+case $(uname -m) in
+    "x86_64")
+        arch="x86_64"
+        ;;
+    "arm64" | "aarch64")
+        arch="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture"
+        exit 1
+        ;;
+esac
 
 fetch() {
     if which curl >/dev/null; then
@@ -26,20 +41,23 @@ fetch() {
     fi
 }
 
+target="${os}_${arch}"
 echo "Detected target: $target"
 
 url=$(
+    # We reverse the output twice to buffer the entire output of curl/wget and
+    # avoid errors when the pipe is closed early.
     fetch https://api.github.com/repos/G-Research/fasttrackml/releases/latest |
-        tac | tac | grep -wo -m1 "https://.*$target.tar.gz" || true
+        $reverse | $reverse | grep -wo -m1 "https://.*$target.tar.gz" || true
 )
 if ! test "$url"; then
     echo "Could not find release info"
     exit 1
 fi
 
-echo "Downloading fml..."
+echo "Downloading FastTrackML..."
 
-temp_dir=$(mktemp -dt fml.tmp)
+temp_dir=$(mktemp -dt fml.XXXXXX)
 trap 'rm -rf "$temp_dir"' EXIT INT TERM
 cd "$temp_dir"
 
@@ -50,12 +68,12 @@ fi
 
 user_bin="$HOME/.local/bin"
 case $PATH in
-*:"$user_bin":* | "$user_bin":* | *:"$user_bin")
-    default_bin=$user_bin
-    ;;
-*)
-    default_bin='/usr/local/bin'
-    ;;
+    *:"$user_bin":* | "$user_bin":* | *:"$user_bin")
+        default_bin=$user_bin
+        ;;
+    *)
+        default_bin='/usr/local/bin'
+        ;;
 esac
 
 _read_installdir() {
@@ -76,12 +94,11 @@ else
 fi
 
 tar xzf fml.tar.gz
-ls
+
 if test -w "$fml_installdir" || [ -n "$FML_BINDIR" ]; then
     mv fml "$fml_installdir/"
 else
     sudo mv fml "$fml_installdir/"
 fi
 
-echo "$("$fml_installdir"/fml --version) has been installed to:"
-echo " • $fml_installdir/fml"
+echo "$("$fml_installdir"/fml --version) has been installed to $fml_installdir/fml"

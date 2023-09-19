dc48cf2f3a24be10c2353f551c4d72e1d36076bb
Geoff Wilson <geoff@gr-oss.io>
2023-09-19 17:53:59 -0400

    Introduce base artifact

--- pkg/api/mlflow/api/request/artifact.go
+++ pkg/api/mlflow/api/request/artifact.go
@@ -1,19 +1,24 @@
 package request
 
-// ListArtifactsRequest is a request object for `GET /mlflow/artifacts/list` endpoint.
-type ListArtifactsRequest struct {
+// BaseArtifactRequest common fields for artifact requests
+type BaseArtifactRequest struct {
 	Path    string `query:"path"`
 	RunID   string `query:"run_id"`
 	RunUUID string `query:"run_uuid"`
 }
 
+// ListArtifactsRequest is a request object for `GET /mlflow/artifacts/list` endpoint.
+type ListArtifactsRequest struct {
+	BaseArtifactRequest
+}
+
 // GetArtifactRequest is for linking to individual artifact item
 type GetArtifactRequest struct {
-	ListArtifactsRequest
+	BaseArtifactRequest
 }
 
 // GetRunID returns Run ID.
-func (r ListArtifactsRequest) GetRunID() string {
+func (r BaseArtifactRequest) GetRunID() string {
 	if r.RunID != "" {
 		return r.RunID
 	}

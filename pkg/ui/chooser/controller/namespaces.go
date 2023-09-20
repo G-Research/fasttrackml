e86a97cc60d7d320be9a29514ab4f01579d0992f
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-09-20 11:40:58 +0000

    removing comment

--- pkg/ui/chooser/controller/namespaces.go
+++ pkg/ui/chooser/controller/namespaces.go
@@ -6,7 +6,6 @@ import (
 
 // GetNamespaces renders the index view
 func (c Controller) GetNamespaces(ctx *fiber.Ctx) error {
-	// namespaceService := namespace.NewService(repositories.NewNamespaceRepository(database.DB))
 	namespaces, err := c.namespaceService.ListNamespaces(ctx.Context())
 	if err != nil {
 		return err

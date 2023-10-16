3a23af5bc4fe41196b6070ae2e3ede1f156b55c5
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-10-16 17:57:57 +0100

    Backport latest changes from `main` to the `features/namespaces` branch (#464)
    I solved the merge conflicts so you didn't have to 😂
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Software Developer <7852635+dsuhinin@users.noreply.github.com>
    Co-authored-by: fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -9,50 +9,50 @@ replace (
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
-	github.com/G-Research/fasttrackml-ui-aim v0.31602.13
+	github.com/G-Research/fasttrackml-ui-aim v0.31602.15
 	github.com/G-Research/fasttrackml-ui-mlflow v0.20301.6
 	github.com/apache/arrow/go/v12 v12.0.1
-	github.com/aws/aws-sdk-go-v2 v1.21.0
-	github.com/aws/aws-sdk-go-v2/config v1.18.40
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.38.5
+	github.com/aws/aws-sdk-go-v2 v1.21.2
+	github.com/aws/aws-sdk-go-v2/config v1.18.45
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.40.2
 	github.com/go-python/gpython v0.2.0
 	github.com/gofiber/fiber/v2 v2.49.2
 	github.com/gofiber/template/html/v2 v2.0.5
 	github.com/google/uuid v1.3.1
-	github.com/hashicorp/golang-lru/v2 v2.0.6
+	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/hetiansu5/urlquery v1.2.7
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/pkg/errors v0.9.1
 	github.com/rotisserie/eris v0.5.4
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.7.0
-	github.com/spf13/viper v1.16.0
+	github.com/spf13/viper v1.17.0
 	github.com/stretchr/testify v1.8.4
-	gorm.io/driver/postgres v1.5.2
+	gorm.io/driver/postgres v1.5.3
 	gorm.io/driver/sqlite v1.4.3
-	gorm.io/gorm v1.25.4
+	gorm.io/gorm v1.25.5
 	gorm.io/plugin/dbresolver v1.4.7
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/apache/thrift v0.16.0 // indirect
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.13 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.38 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.11 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.41 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.35 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.42 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.1.4 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.14 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.36 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.35 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.15.4 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.14.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.16.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.22.0 // indirect
-	github.com/aws/smithy-go v1.14.2 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.14 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.43 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.43 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.37 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.45 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.1.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.38 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.37 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.15.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.15.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.17.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.23.2 // indirect
+	github.com/aws/smithy-go v1.15.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/gofiber/template v1.8.2 // indirect
@@ -63,11 +63,11 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.3.1 // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/asmfmt v1.3.2 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -76,25 +76,30 @@ require (
 	github.com/minio/asm2plan9s v0.0.0-20200509001527-cdd76441f9d8 // indirect
 	github.com/minio/c2goasm v0.0.0-20190812172519-36a3d3bbc4f3 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rivo/uniseg v0.3.4 // indirect
-	github.com/spf13/afero v1.9.5 // indirect
+	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.10.0 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/tools v0.9.1 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,164 +1,176 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+module.exports = async function configCreatorAsync() {
+    const lightCodeTheme = require('prism-react-renderer/themes/github');
+    const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-    title: 'FastTrackML',
-    tagline: 'Experiment tracking server focused on speed and scalability',
-    favicon: 'favicon.ico',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
+    // Get latest release version from GitHub
+    const { Octokit } = require('@octokit/rest');
+    const octokit = new Octokit();
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/releases/latest',
+        {
+            owner: 'G-Research',
+            repo: 'fasttrackml'
+        }
+    );
+    const releaseVersion = data.tag_name.substring(1);
+    const releaseUrl = data.html_url;
 
-    url: 'https://fasttrackml.io',
-    baseUrl: '/',
+    /** @type {import('@docusaurus/types').Config} */
+    return {
+        title: 'FastTrackML',
+        tagline: 'Experiment tracking server focused on speed and scalability',
+        favicon: 'favicon.ico',
+        onBrokenLinks: 'throw',
+        onBrokenMarkdownLinks: 'warn',
 
-    organizationName: 'G-Research',
-    projectName: 'fasttrackml',
+        url: 'https://fasttrackml.io',
+        baseUrl: '/',
 
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
+        organizationName: 'G-Research',
+        projectName: 'fasttrackml',
 
-    customFields: {
-        email: 'fasttrackml@gr-oss.io',
-        newIssueUrl: 'https://github.com/G-Research/fasttrackml/issues/new',
-    },
+        i18n: {
+            defaultLocale: 'en',
+            locales: ['en'],
+        },
 
-    presets: [
-        [
-            'classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: false, // disabling docs
-                theme: {
-                    customCss: [require.resolve('./src/css/theming.css'), require.resolve('./src/css/announcement-bar.css'), require.resolve('./src/css/global.css')],
-                },
-                gtag: {
-                    trackingID: 'G-2YZLJEB3PY',
-                    anonymizeIP: true,
-                },
-            }),
-        ],
-    ],
+        customFields: {
+            email: 'fasttrackml@gr-oss.io',
+            newIssueUrl: 'https://github.com/G-Research/fasttrackml/issues/new',
+        },
 
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            image: 'images/project-social-preview.png', // project's social card
-            navbar: {
-                logo: {
-                    alt: 'FastTrackML logo',
-                    src: 'logo/project-logo-light.svg',
-                    srcDark: 'logo/project-logo-dark.svg',
-                    width: 140,
-                },
-                items: [
-                    // left
-                    {
-                        to: '/#quickstart',
-                        label: 'Quickstart',
-                        position: 'left',
-                        activeBaseRegex: `dummy-never-match`,
+        presets: [
+            [
+                'classic',
+                /** @type {import('@docusaurus/preset-classic').Options} */
+                ({
+                    docs: false, // disabling docs
+                    theme: {
+                        customCss: [require.resolve('./src/css/theming.css'), require.resolve('./src/css/announcement-bar.css'), require.resolve('./src/css/global.css')],
                     },
-                    {
-                        to: '/#contact-us',
-                        label: 'Contact Us',
-                        position: 'left',
-                        activeBaseRegex: `dummy-never-match`,
+                    gtag: {
+                        trackingID: 'G-2YZLJEB3PY',
+                        anonymizeIP: true,
                     },
-                    // right
-                    {
-                        href: 'https://github.com/G-Research/fasttrackml',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                    {
-                        href: 'https://twitter.com/oss_gr',
-                        label: 'Twitter',
-                        position: 'right',
-                    },
-                ],
-            },
-            footer: {
-                links: [
-                    {
-                        title: 'Links',
-                        items: [
-                            {
-                                label: 'Quickstart',
-                                to: '/#quickstart',
-                            },
-                            {
-                                label: `Contact Us`,
-                                to: '/#contact-us',
-                            },
-                            {
-                                label: `Report an Issue`,
-                                to: 'https://github.com/G-Research/fasttrackml/issues',
-                            },
-                            {
-                                label: `Create a Pull Request`,
-                                to: 'https://github.com/G-Research/fasttrackml/pulls',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/G-Research/fasttrackml',
-                            },
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/oss_gr',
-                            },
-                            {
-                                label: 'G-Research Open-Source',
-                                href: 'https://opensource.gresearch.co.uk/',
-                            },
-                        ],
-                    },
-                ],
-                logo: {
-                    alt: 'G-Research Open-Source Software',
-                    src: 'logo/organization.svg',
-                    srcDark: 'logo/organization-dark.svg',
-                    href: 'https://opensource.gresearch.co.uk/',
-                },
-                copyright: `Copyright © ${new Date().getFullYear()} G-Research`,
-            },
-            announcementBar: {
-                // https://docusaurus.io/docs/api/themes/configuration#announcement-bar
-                id: 'announcement-bar',
-                content: `FastTrackML 0.3.1 has been <a href="https://github.com/G-Research/fasttrackml/releases/tag/v0.3.1" target="_blank">released</a>!`,
-                isCloseable: true,
-            },
-            colorMode: {
-                defaultMode: 'light',
-                disableSwitch: false,
-                respectPrefersColorScheme: true,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-                defaultLanguage: 'bash',
-                additionalLanguages: ['python', 'powershell'],
-            },
-            metadata: [
-                {
-                    name: 'twitter:card', content: 'summary'
-                },
-                {
-                    name: 'keywords',
-                    content: 'machine learning, experiment tracking, mlflow, mlflow tracking server, fasttrackml'
-                },
+                }),
             ],
-        }),
-};
+        ],
 
-module.exports = config;
+        themeConfig:
+            /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+            ({
+                image: 'images/project-social-preview.png', // project's social card
+                navbar: {
+                    logo: {
+                        alt: 'FastTrackML logo',
+                        src: 'logo/project-logo-light.svg',
+                        srcDark: 'logo/project-logo-dark.svg',
+                        width: 140,
+                    },
+                    items: [
+                        // left
+                        {
+                            to: '/#quickstart',
+                            label: 'Quickstart',
+                            position: 'left',
+                            activeBaseRegex: `dummy-never-match`,
+                        },
+                        {
+                            to: '/#contact-us',
+                            label: 'Contact Us',
+                            position: 'left',
+                            activeBaseRegex: `dummy-never-match`,
+                        },
+                        // right
+                        {
+                            href: 'https://github.com/G-Research/fasttrackml',
+                            label: 'GitHub',
+                            position: 'right',
+                        },
+                        {
+                            href: 'https://twitter.com/oss_gr',
+                            label: 'Twitter',
+                            position: 'right',
+                        },
+                    ],
+                },
+                footer: {
+                    links: [
+                        {
+                            title: 'Links',
+                            items: [
+                                {
+                                    label: 'Quickstart',
+                                    to: '/#quickstart',
+                                },
+                                {
+                                    label: `Contact Us`,
+                                    to: '/#contact-us',
+                                },
+                                {
+                                    label: `Report an Issue`,
+                                    to: 'https://github.com/G-Research/fasttrackml/issues',
+                                },
+                                {
+                                    label: `Create a Pull Request`,
+                                    to: 'https://github.com/G-Research/fasttrackml/pulls',
+                                },
+                            ],
+                        },
+                        {
+                            title: 'More',
+                            items: [
+                                {
+                                    label: 'GitHub',
+                                    href: 'https://github.com/G-Research/fasttrackml',
+                                },
+                                {
+                                    label: 'Twitter',
+                                    href: 'https://twitter.com/oss_gr',
+                                },
+                                {
+                                    label: 'G-Research Open-Source',
+                                    href: 'https://opensource.gresearch.co.uk/',
+                                },
+                            ],
+                        },
+                    ],
+                    logo: {
+                        alt: 'G-Research Open-Source Software',
+                        src: 'logo/organization.svg',
+                        srcDark: 'logo/organization-dark.svg',
+                        href: 'https://opensource.gresearch.co.uk/',
+                    },
+                    copyright: `Copyright © ${new Date().getFullYear()} G-Research`,
+                },
+                announcementBar: {
+                    // https://docusaurus.io/docs/api/themes/configuration#announcement-bar
+                    id: `announcement-bar-${releaseVersion}`,
+                    content: `FastTrackML ${releaseVersion} has been <a href="${releaseUrl}" target="_blank">released</a>!`,
+                    isCloseable: true,
+                },
+                colorMode: {
+                    defaultMode: 'light',
+                    disableSwitch: false,
+                    respectPrefersColorScheme: true,
+                },
+                prism: {
+                    theme: lightCodeTheme,
+                    darkTheme: darkCodeTheme,
+                    defaultLanguage: 'bash',
+                    additionalLanguages: ['python', 'powershell'],
+                },
+                metadata: [
+                    {
+                        name: 'twitter:card', content: 'summary'
+                    },
+                    {
+                        name: 'keywords',
+                        content: 'machine learning, experiment tracking, mlflow, mlflow tracking server, fasttrackml'
+                    },
+                ],
+            }),
+    };
+};

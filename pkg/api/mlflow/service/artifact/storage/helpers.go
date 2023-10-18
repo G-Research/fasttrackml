bb75722d2251435da853565729f5669815308795
dsuhinin <suhinin.dmitriy@gmail.com>
2023-10-18 15:46:55 +0200

    *fix typos.

--- pkg/api/mlflow/service/artifact/storage/helpers.go
+++ pkg/api/mlflow/service/artifact/storage/helpers.go
@@ -8,7 +8,7 @@ import (
 )
 
 // ExtractBucketAndPrefix extracts bucket name and prefix path from provided uri.
-// uri could be any S3 compatible storage uri like; AWS S3 or Google Storage.
+// uri could be any S3 compatible storage uri like AWS S3 or Google Storage.
 // after processing of uri s3://fasttrackml/2/30357ed2eaac4f2cacdbcd0e06e9e48a/artifacts result will be:
 // - bucket = fasttrackml
 // - prefix = 2/30357ed2eaac4f2cacdbcd0e06e9e48a/artifacts

c616d3de4a2a6ba4889dccc00658a8f10b2c30ba
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-10-19 08:16:44 +0100

    Fix wrong Y axis scale in Aim UI when metric has infinity values (#494)

--- pkg/api/aim/runs.go
+++ pkg/api/aim/runs.go
@@ -1032,6 +1032,12 @@ func DeleteBatch(c *fiber.Ctx) error {
 func toNumpy(values []float64) fiber.Map {
 	buf := bytes.NewBuffer(make([]byte, 0, len(values)*8))
 	for _, v := range values {
+		switch v {
+		case math.MaxFloat64:
+			v = math.Inf(1)
+		case -math.MaxFloat64:
+			v = math.Inf(-1)
+		}
 		binary.Write(buf, binary.LittleEndian, v)
 	}
 	return fiber.Map{

4b66f15c85bfe6a51e9189688d5e5c291f367d6f
Geoffrey Wilson <geoff@gr-oss.io>
2023-10-20 09:30:04 -0400

    VSCode settings improvements (#431)
    * Tweaks to devcontainer/vscode setup for integration test support
    
    * Fix launch issue when current file is a test
    
    * Restore previous sqlite file config and add new integration tests
    
    * create s3 buckets used by integration tests, ignoring errors
    
    * revert bucket creation in compose file
    
    * fix errant quote mark
    
    * rename the integration tests server launch
    
    * nolint errcheck plus comment
    
    * remove s3 buckets after test
    
    * Remove bucket creation from tests/integration compose file; ignore errors creating buckets
    
    * nolint also requires gosec
    
    * fixes for s3 helper from PR
    
    * use pagination to fetch object keys
    
    * check err from helpers.CreateBuckets
    
    * wait up to 10 secs for bucket delete
    
    ---------
    
    Co-authored-by: Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>

--- tests/integration/golang/helpers/s3.go
+++ tests/integration/golang/helpers/s3.go
@@ -2,12 +2,16 @@ package helpers
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/rotisserie/eris"
 )
+
+var testBuckets = []string{"bucket1", "bucket2"}
 
 // NewS3Client creates new instance of S3 client.
 func NewS3Client(endpoint string) (*s3.Client, error) {
@@ -30,4 +34,72 @@ func NewS3Client(endpoint string) (*s3.Client, error) {
 	return s3.NewFromConfig(cfg, func(o *s3.Options) {
 		o.UsePathStyle = true
 	}), nil
+}
+
+// CreateBuckets creates the test bucekts.
+func CreateBuckets(s3Client *s3.Client) error {
+	for _, bucket := range testBuckets {
+		_, err := s3Client.CreateBucket(context.Background(), &s3.CreateBucketInput{
+			Bucket: aws.String(bucket),
+		})
+		if err != nil {
+			return eris.Wrapf(err, "failed to create bucket '%s'", bucket)
+		}
+	}
+	return nil
+}
+
+// RemoveBuckets removes the test buckets.
+func RemoveBuckets(s3Client *s3.Client) error {
+	for _, bucket := range testBuckets {
+		if err := removeBucket(s3Client, bucket); err != nil {
+			return eris.Wrapf(err, "failed to remove bucket '%s'", bucket)
+		}
+	}
+	return nil
+}
+
+// removeBucket removes a bucket and its objects.
+func removeBucket(s3Client *s3.Client, bucket string) error {
+	// Delete all objects in the bucket
+	var objectIDs []types.ObjectIdentifier
+	paginator := s3.NewListObjectsV2Paginator(s3Client, &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+	})
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(context.Background())
+		if err != nil {
+			return eris.Wrapf(err, "error paging objects in bucket '%s'", bucket)
+		}
+		for _, object := range page.Contents {
+			objectIDs = append(objectIDs, types.ObjectIdentifier{Key: object.Key})
+		}
+	}
+	if len(objectIDs) > 0 {
+		_, err := s3Client.DeleteObjects(context.Background(), &s3.DeleteObjectsInput{
+			Bucket: aws.String(bucket),
+			Delete: &types.Delete{Objects: objectIDs},
+		})
+		if err != nil {
+			return eris.Wrapf(err, "failed to delete objects in bucket '%s'", bucket)
+		}
+	}
+
+	// Delete the bucket
+	if _, err := s3Client.DeleteBucket(context.Background(), &s3.DeleteBucketInput{
+		Bucket: aws.String(bucket),
+	}); err != nil {
+		return eris.Wrapf(err, "failed to delete bucket '%s'", bucket)
+	}
+	waiter := s3.NewBucketNotExistsWaiter(s3Client)
+	if err := waiter.Wait(
+		context.Background(),
+		&s3.HeadBucketInput{
+			Bucket: aws.String(bucket),
+		},
+		time.Second*10,
+	); err != nil {
+		return eris.Wrapf(err, "error waiting for bucket '%s' deletion", bucket)
+	}
+	return nil
 }

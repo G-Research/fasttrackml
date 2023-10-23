de54c6b95edf93b98641cf2e49235d8cc4ea70eb
Geoffrey Wilson <g@wlsn.us>
2023-10-23 01:57:58 +0000

    add test to run migrations against mlflow-created db

---
+++ tests/integration/golang/database/migrate_test.go
@@ -0,0 +1,41 @@
+//go:build integration
+
+package database
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/G-Research/fasttrackml/pkg/database"
+)
+
+func TestMigrate(t *testing.T) {
+	// copy the original mlflow db to a temp file
+	mlflowDbPath := t.TempDir()
+	src, err := os.Open("mlflow.db")
+	assert.Nil(t, err)
+	defer src.Close()
+
+	dst, err := os.Create(fmt.Sprintf("%s/mlflow.db", mlflowDbPath))
+	assert.Nil(t, err)
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	assert.Nil(t, err)
+
+	// make DbProvider using the temp copy
+	db, err := database.NewDBProvider(
+		fmt.Sprintf("sqlite://%s/mlflow.db", mlflowDbPath),
+		1*time.Second,
+		20,
+	)
+	assert.Nil(t, err)
+
+	// run migrations
+	assert.Nil(t, database.CheckAndMigrateDB(true, db.GormDB()))
+}

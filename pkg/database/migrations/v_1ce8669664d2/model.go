fc08723ada3ac4e7dae9740a7bab709320911d5e
Geoffrey Wilson <geoff@gr-oss.io>
2023-10-31 15:31:15 +0000

    goformat

--- pkg/database/migrations/v_1ce8669664d2/model.go
+++ pkg/database/migrations/v_1ce8669664d2/model.go
@@ -31,12 +31,12 @@ const (
 )
 
 type Experiment struct {
-	ID               *int32          `gorm:"column:experiment_id;not null;primaryKey"`
-	Name             string          `gorm:"type:varchar(256);not null;unique"`
-	ArtifactLocation string          `gorm:"type:varchar(256)"`
-	LifecycleStage   LifecycleStage  `gorm:"type:varchar(32);check:lifecycle_stage IN ('active', 'deleted')"`
-	CreationTime     sql.NullInt64   `gorm:"type:bigint"`
-	LastUpdateTime   sql.NullInt64   `gorm:"type:bigint"`
+	ID               *int32         `gorm:"column:experiment_id;not null;primaryKey"`
+	Name             string         `gorm:"type:varchar(256);not null;unique"`
+	ArtifactLocation string         `gorm:"type:varchar(256)"`
+	LifecycleStage   LifecycleStage `gorm:"type:varchar(32);check:lifecycle_stage IN ('active', 'deleted')"`
+	CreationTime     sql.NullInt64  `gorm:"type:bigint"`
+	LastUpdateTime   sql.NullInt64  `gorm:"type:bigint"`
 	Tags             []ExperimentTag
 	Runs             []Run
 }

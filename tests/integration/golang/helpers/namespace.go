750f899a40c1a6cd79a2c584cd7f9b0ae41187c1
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-11-10 14:34:02 +0100

    Admin endpoint integration tests (#560)

---
+++ tests/integration/golang/helpers/namespace.go
@@ -0,0 +1,23 @@
+package helpers
+
+import (
+	"github.com/G-Research/fasttrackml/pkg/api/mlflow/dao/models"
+	"github.com/G-Research/fasttrackml/pkg/ui/admin/request"
+)
+
+// CheckExperiments checks if the requested namespaces are present in the expected namespaces.
+func CheckNamespaces(expectedNamespaces []models.Namespace, requestedNamespaces []request.Namespace) bool {
+	for _, testNamespace := range requestedNamespaces {
+		found := false
+		for _, namespace := range expectedNamespaces {
+			if namespace.Code == testNamespace.Code && namespace.Description == testNamespace.Description {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}

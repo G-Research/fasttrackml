84be14a9a39bf6792857c336d5f7ce5989577672
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-11-20 13:27:54 +0000

    fix test convert

--- pkg/api/mlflow/dao/convertors/metric_test.go
+++ pkg/api/mlflow/dao/convertors/metric_test.go
@@ -2,10 +2,15 @@ package convertors
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/G-Research/fasttrackml/pkg/api/mlflow/api/request"
 )
 
 func TestConvertMetricParamRequestToDBModel(t *testing.T) {
-	/*req := request.LogMetricRequest{
+	req := request.LogMetricRequest{
 		Key:       "key",
 		Step:      1,
 		Value:     1.1,
@@ -16,12 +21,11 @@ func TestConvertMetricParamRequestToDBModel(t *testing.T) {
 			"value": "testValue",
 		},
 	}
-	result, err := ConvertMetricParamRequestToDBModel("run_id", &req)
+	result, err := ConvertMetricParamRequestToDBModel("run_id", nil, &req)
 	require.Nil(t, err)
 	assert.Equal(t, "key", result.Key)
 	assert.Equal(t, int64(1), result.Step)
 	assert.Equal(t, 1.1, result.Value)
 	assert.Equal(t, "run_id", result.RunID)
 	assert.Equal(t, int64(1234567890), result.Timestamp)
-	*/
 }

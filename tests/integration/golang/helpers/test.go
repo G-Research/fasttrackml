b371762452289ed61d508870803ed35fd3326b13
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-11-23 12:08:14 +0000

    Refactor BaseTestSuite helper to provide setup/teardown

--- tests/integration/golang/helpers/test.go
+++ tests/integration/golang/helpers/test.go
@@ -1,11 +1,14 @@
 package helpers
 
 import (
+	"context"
 	"time"
 
 	"github.com/stretchr/testify/suite"
 	"gorm.io/gorm"
 
+	"github.com/G-Research/fasttrackml/pkg/api/mlflow/common"
+	"github.com/G-Research/fasttrackml/pkg/api/mlflow/dao/models"
 	"github.com/G-Research/fasttrackml/pkg/database"
 	"github.com/G-Research/fasttrackml/tests/integration/golang/fixtures"
 )
@@ -14,21 +17,28 @@ var db *gorm.DB
 
 type BaseTestSuite struct {
 	suite.Suite
-	AIMClient          func() *HttpClient
-	MlflowClient       func() *HttpClient
-	AdminClient        func() *HttpClient
-	AppFixtures        *fixtures.AppFixtures
-	RunFixtures        *fixtures.RunFixtures
-	TagFixtures        *fixtures.TagFixtures
-	MetricFixtures     *fixtures.MetricFixtures
-	ParamFixtures      *fixtures.ParamFixtures
-	ProjectFixtures    *fixtures.ProjectFixtures
-	DashboardFixtures  *fixtures.DashboardFixtures
-	ExperimentFixtures *fixtures.ExperimentFixtures
-	NamespaceFixtures  *fixtures.NamespaceFixtures
+	AIMClient                   func() *HttpClient
+	MlflowClient                func() *HttpClient
+	AdminClient                 func() *HttpClient
+	AppFixtures                 *fixtures.AppFixtures
+	RunFixtures                 *fixtures.RunFixtures
+	TagFixtures                 *fixtures.TagFixtures
+	MetricFixtures              *fixtures.MetricFixtures
+	ParamFixtures               *fixtures.ParamFixtures
+	ProjectFixtures             *fixtures.ProjectFixtures
+	DashboardFixtures           *fixtures.DashboardFixtures
+	ExperimentFixtures          *fixtures.ExperimentFixtures
+	DefaultExperiment           *models.Experiment
+	NamespaceFixtures           *fixtures.NamespaceFixtures
+	DefaultNamespace            *models.Namespace
+	ResetOnSubTest              bool
+	SkipCreateDefaultNamespace  bool
+	SkipCreateDefaultExperiment bool
+	setupHooks                  []func()
+	tearDownHooks               []func()
 }
 
-func (s *BaseTestSuite) SetupTest() {
+func (s *BaseTestSuite) SetupSuite() {
 	if db == nil {
 		instance, err := database.NewDBProvider(
 			GetDatabaseUri(),
@@ -85,6 +95,80 @@ func (s *BaseTestSuite) SetupTest() {
 	s.Require().Nil(err)
 	s.TagFixtures = tagFixtures
 
-	// by default, unload everything.
+	s.AddSetupHook(s.setup)
+	s.AddTearDownHook(s.tearDown)
+}
+
+func (s *BaseTestSuite) setup() {
 	s.Require().Nil(s.NamespaceFixtures.UnloadFixtures())
+
+	if !s.SkipCreateDefaultNamespace {
+		var err error
+		s.DefaultNamespace, err = s.NamespaceFixtures.CreateNamespace(context.Background(), &models.Namespace{
+			Code:                "default",
+			DefaultExperimentID: common.GetPointer(int32(0)),
+		})
+		s.Require().Nil(err)
+
+		if !s.SkipCreateDefaultExperiment {
+			s.DefaultExperiment, err = s.ExperimentFixtures.CreateExperiment(context.Background(), &models.Experiment{
+				Name:           "Default",
+				LifecycleStage: models.LifecycleStageActive,
+				NamespaceID:    s.DefaultNamespace.ID,
+			})
+			s.Require().Nil(err)
+
+			s.DefaultNamespace.DefaultExperimentID = s.DefaultExperiment.ID
+			_, err = s.NamespaceFixtures.UpdateNamespace(context.Background(), s.DefaultNamespace)
+			s.Require().Nil(err)
+		}
+	}
+}
+
+func (s *BaseTestSuite) AddSetupHook(hook func()) {
+	s.setupHooks = append(s.setupHooks, hook)
+}
+
+func (s *BaseTestSuite) runSetupHooks() {
+	for _, hook := range s.setupHooks {
+		hook()
+	}
+}
+
+func (s *BaseTestSuite) SetupTest() {
+	if !s.ResetOnSubTest {
+		s.runSetupHooks()
+	}
+}
+
+func (s *BaseTestSuite) SetupSubTest() {
+	if s.ResetOnSubTest {
+		s.runSetupHooks()
+	}
+}
+
+func (s *BaseTestSuite) AddTearDownHook(hook func()) {
+	s.tearDownHooks = append([]func(){hook}, s.tearDownHooks...)
+}
+
+func (s *BaseTestSuite) tearDown() {
+	s.Require().Nil(s.NamespaceFixtures.UnloadFixtures())
+}
+
+func (s *BaseTestSuite) runTearDownHooks() {
+	for _, hook := range s.tearDownHooks {
+		hook()
+	}
+}
+
+func (s *BaseTestSuite) TearDownTest() {
+	if !s.ResetOnSubTest {
+		s.runTearDownHooks()
+	}
+}
+
+func (s *BaseTestSuite) TearDownSubTest() {
+	if s.ResetOnSubTest {
+		s.runTearDownHooks()
+	}
 }

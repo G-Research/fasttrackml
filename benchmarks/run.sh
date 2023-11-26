9129d53cacdd4c1e5124f7b0bda681b01047a5a2
asuzukosi <keloasuzu@yahoo.com>
2023-11-26 12:50:42 +0000

    feat: add bash command to run the scripts

---
+++ benchmarks/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker first."
+    exit 1
+fi
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is not installed. Please install Docker Compose first."
+    exit 1
+fi
+
+# Run your specific Docker command
+docker-Compose up performance_benchmark_test

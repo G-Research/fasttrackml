f1ca273fe1f6c151f24b13ee74b1b3ab9b11f011
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-12-06 17:20:50 +0000

    Parallelize integration tests (#689)

--- tests/integration/golang/helpers/env.go
+++ tests/integration/golang/helpers/env.go
@@ -2,6 +2,8 @@ package helpers
 
 import "os"
 
+const defaultDatabaseBackend = "sqlite"
+
 func GetLogLevel() string {
 	level, ok := os.LookupEnv("FML_LOG_LEVEL")
 	if ok {
@@ -10,28 +12,36 @@ func GetLogLevel() string {
 	return "info"
 }
 
-func GetDatabaseUri() string {
-	uri, ok := os.LookupEnv("FML_DATABASE_URI")
+func GetDatabaseBackend() string {
+	uri, ok := os.LookupEnv("FML_DATABASE_BACKEND")
 	if ok {
 		return uri
 	}
-	return "sqlite:///tmp/fasttrackml.db"
+	return defaultDatabaseBackend
 }
 
-func GetServiceUri() string {
-	uri, ok := os.LookupEnv("FML_SERVICE_URI")
+func GetInputDatabaseBackend() string {
+	uri, ok := os.LookupEnv("FML_INPUT_DATABASE_BACKEND")
 	if ok {
 		return uri
 	}
-	return "http://localhost:5000"
+	return defaultDatabaseBackend
 }
 
-func GetS3EndpointUri() string {
-	uri, ok := os.LookupEnv("FML_S3_ENDPOINT_URI")
+func GetOutputDatabaseBackend() string {
+	uri, ok := os.LookupEnv("FML_OUTPUT_DATABASE_BACKEND")
 	if ok {
 		return uri
 	}
-	return "http://localhost:9000"
+	return defaultDatabaseBackend
+}
+
+func GetPostgresUri() string {
+	uri, ok := os.LookupEnv("FML_POSTGRES_URI")
+	if ok {
+		return uri
+	}
+	return "postgres://postgres:postgres@localhost/postgres"
 }
 
 func GetGSEndpointUri() string {
@@ -42,18 +52,10 @@ func GetGSEndpointUri() string {
 	return "http://localhost:4443/storage/v1/"
 }
 
-func GetInputDatabaseUri() string {
-	uri, ok := os.LookupEnv("FML_INPUT_DATABASE_URI")
+func GetS3EndpointUri() string {
+	uri, ok := os.LookupEnv("FML_S3_ENDPOINT_URI")
 	if ok {
 		return uri
 	}
-	return "sqlite:///tmp/fasttrackml-in.db"
-}
-
-func GetOutputDatabaseUri() string {
-	uri, ok := os.LookupEnv("FML_OUTPUT_DATABASE_URI")
-	if ok {
-		return uri
-	}
-	return "sqlite:///tmp/fasttrackml-out.db"
+	return "http://localhost:9000"
 }

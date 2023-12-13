7eef1dcb0df87ea05feff1640bad185828b480b8
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-12-13 10:59:05 +0000

    Simplify app and dashboard creation in integration tests (#737)

--- tests/integration/golang/aim/dashboard/get_dashboards_test.go
+++ tests/integration/golang/aim/dashboard/get_dashboards_test.go
@@ -5,13 +5,10 @@ package run
 import (
 	"context"
 	"testing"
-	"time"
 
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/G-Research/fasttrackml/pkg/api/aim/response"
-	"github.com/G-Research/fasttrackml/pkg/database"
 	"github.com/G-Research/fasttrackml/tests/integration/golang/helpers"
 )
 
@@ -43,20 +40,8 @@ func (s *GetDashboardsTestSuite) Test_Ok() {
 	}
 	for _, tt := range tests {
 		s.Run(tt.name, func() {
-			app, err := s.AppFixtures.CreateApp(context.Background(), &database.App{
-				Base: database.Base{
-					ID:         uuid.New(),
-					IsArchived: false,
-					CreatedAt:  time.Now(),
-				},
-				Type:        "mpi",
-				State:       database.AppState{},
-				NamespaceID: s.DefaultNamespace.ID,
-			})
-			s.Require().Nil(err)
-
 			dashboards, err := s.DashboardFixtures.CreateDashboards(
-				context.Background(), tt.expectedDashboardCount, &app.ID,
+				context.Background(), s.DefaultNamespace, tt.expectedDashboardCount,
 			)
 			s.Require().Nil(err)
 
@@ -65,8 +50,8 @@ func (s *GetDashboardsTestSuite) Test_Ok() {
 			s.Equal(tt.expectedDashboardCount, len(resp))
 			for idx := 0; idx < tt.expectedDashboardCount; idx++ {
 				s.Equal(dashboards[idx].ID, resp[idx].ID)
-				s.Equal(app.ID, resp[idx].AppID)
-				s.Equal(app.Type, resp[idx].AppType)
+				s.Equal(dashboards[idx].App.ID, resp[idx].AppID)
+				s.Equal(dashboards[idx].App.Type, resp[idx].AppType)
 				s.Equal(dashboards[idx].Name, resp[idx].Name)
 				s.Equal(dashboards[idx].Description, resp[idx].Description)
 				s.NotEmpty(resp[idx].CreatedAt)

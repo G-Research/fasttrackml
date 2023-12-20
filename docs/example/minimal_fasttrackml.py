9b50b4be9026f972bad6f11fd7eef3e221c7d1ac
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-12-20 17:18:56 +0000

    format python

--- docs/example/minimal_fasttrackml.py
+++ docs/example/minimal_fasttrackml.py
@@ -16,12 +16,12 @@ def main():
     experiment_id = "0"
     run = client.create_run(experiment_id)
     run_id = run.info.run_id
-    
+
     # Logging a parameter
     param_key = "param1"
     param_value = randint(0, 100)
     client.log_param(run_id, param_key, param_value)
-    
+
     metric_key = "foo"
     # Logging metrics with context
     client.log_metric(run_id, metric_key, random(), context={"context_key": "context_value1"})
@@ -31,11 +31,15 @@ def main():
 
     # Logging a batch of metrics
     timestamp = int(time.time() * 1000)
-    metrics = [Metric("loss", 0.2, timestamp, 1, context={"context_key": "context_value3"}), Metric("precision", 0.92, timestamp, 1, context={"context_key": "context_value4"})]
+    metrics = [
+        Metric("loss", 0.2, timestamp, 1, context={"context_key": "context_value3"}),
+        Metric("precision", 0.92, timestamp, 1, context={"context_key": "context_value4"}),
+    ]
     client.log_batch(run_id, metrics=metrics)
 
     # Closing the run
     client.set_terminated(run_id)
+
 
 if __name__ == "__main__":
     main()

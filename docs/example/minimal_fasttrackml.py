5bc0c9c08a5a76030ddc0b18b048ac9830249132
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2023-12-20 17:53:10 +0000

    format python example

--- docs/example/minimal_fasttrackml.py
+++ docs/example/minimal_fasttrackml.py
@@ -16,6 +16,7 @@ def print_metric_info(history):
         print(f"context: {m.context}")
         print("--")
 
+
 def main():
     fasttrackml.set_tracking_uri("http://localhost:5000")
     # Creating an instance of the Fasttrackml client
@@ -25,12 +26,12 @@ def main():
     experiment_id = "0"
     run = client.create_run(experiment_id)
     run_id = run.info.run_id
-    
+
     # Logging a parameter
     param_key = "param1"
     param_value = randint(0, 100)
     client.log_param(run_id, param_key, param_value)
-    
+
     metric_key = "foo"
     # Logging metrics with context
     client.log_metric(run_id, metric_key, random(), context={"context_key": "context_value1"})
@@ -40,19 +41,23 @@ def main():
 
     # Logging a batch of metrics
     timestamp = int(time.time() * 1000)
-    metrics = [Metric("loss", 0.2, timestamp, 1, context={"context_key": "context_value3"}), Metric("precision", 0.92, timestamp, 1, context={"context_key": "context_value4"})]
+    metrics = [
+        Metric("loss", 0.2, timestamp, 1, context={"context_key": "context_value3"}),
+        Metric("precision", 0.92, timestamp, 1, context={"context_key": "context_value4"}),
+    ]
     client.log_batch(run_id, metrics=metrics)
-    
+
     # Retrieving metric histories
-    metric_keys = [metric_key, "loss"] 
+    metric_keys = [metric_key, "loss"]
     metric_histories_df = client.get_metric_histories(run_ids=[run_id], metric_keys=metric_keys)
 
     print(metric_histories_df)
-    
+
     print_metric_info(client.get_metric_history(run_id, metric_key))
 
     # Closing the run
     client.set_terminated(run_id)
+
 
 if __name__ == "__main__":
     main()

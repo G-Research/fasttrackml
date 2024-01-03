be16a838bfb984d7d16490ebeea69c2295e79c17
Geoff Wilson <geoff@gr-oss.io>
2024-01-03 17:54:55 -0500

    Drop old indexes

--- pkg/database/migrations/v_0009/migrate.go
+++ pkg/database/migrations/v_0009/migrate.go
@@ -1,6 +1,8 @@
 package v_0009
 
 import (
+	"fmt"
+
 	"github.com/rotisserie/eris"
 	"gorm.io/gorm"
 
@@ -12,13 +14,20 @@ const Version = "2c2299e4e061"
 func Migrate(db *gorm.DB) error {
 	return migrations.RunWithoutForeignKeyIfNeeded(db, func() error {
 		return db.Transaction(func(tx *gorm.DB) error {
-			// Rename the existing tables for backup
-			tables := []string{ "metrics", "latest_metrics"}
+			// Rename the existing tables and indexes
+			tables := []string{"metrics", "latest_metrics"}
 			for _, table := range tables {
-				backup_name := fmt.Sprintf("%s_old", table)
-				if err := tx.Migrator().RenameTable(table, backup_name); err != nil {
+				index := fmt.Sprintf("idx_%s_run_id", table)
+				if err := tx.Migrator().DropIndex(table, index); err != nil {
+					return eris.Wrapf(err, "error renaming %s", index)
+				}
+				if err := tx.Migrator().RenameTable(table, backupName(table)); err != nil {
 					return eris.Wrapf(err, "error renaming %s", table)
 				}
+			}
+			index := "idx_metrics_iter"
+			if err := tx.Migrator().DropIndex(backupName("metrics"), index); err != nil {
+				return eris.Wrapf(err, "error renaming %s", index)
 			}
 
 			// Auto-migrate the new tables
@@ -27,14 +36,14 @@ func Migrate(db *gorm.DB) error {
 			}
 
 			// Copy the data from the old tables to the new ones
-			for _, table := tables {
-				if err := tx.Exec("INSERT INTO metrics SELECT *, '0' FROM old_metrics").Error; err != nil {
+			for _, table := range tables {
+				if err := tx.Exec(fmt.Sprintf("INSERT INTO %s SELECT *, '0' FROM %s", table, backupName(table))).Error; err != nil {
 					return eris.Wrapf(err, "error copying data for %s", table)
 				}
 
 				// Drop the backup tables
-				if err := tx.Exec("DROP TABLE old_metrics").Error; err != nil {
-					return eris.Wrapf(err, "error dropping %s", table)
+				if err := tx.Exec("DROP TABLE " + backupName(table)).Error; err != nil {
+					return eris.Wrapf(err, "error dropping %s", backupName(table))
 				}
 			}
 
@@ -45,4 +54,8 @@ func Migrate(db *gorm.DB) error {
 				Error
 		})
 	})
+}
+
+func backupName(name string) string {
+	return fmt.Sprintf("%s_old", name)
 }

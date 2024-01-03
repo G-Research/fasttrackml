c9cc2bb5369fd034a71d34d8d5e104a26e60f75c
Geoff Wilson <geoff@gr-oss.io>
2024-01-03 15:18:56 -0500

    Use AutoMigrate and new unique index for context_id col

--- pkg/database/migrations/v_0009/migrate.go
+++ pkg/database/migrations/v_0009/migrate.go
@@ -12,16 +12,13 @@ const Version = "2c2299e4e061"
 func Migrate(db *gorm.DB) error {
 	return migrations.RunWithoutForeignKeyIfNeeded(db, func() error {
 		return db.Transaction(func(tx *gorm.DB) error {
-			if err := tx.Migrator().AutoMigrate(&Context{}); err != nil {
-				return err
-			}
-
 			for _, model := range []interface{}{
+				&Context{},
 				&Metric{},
 				&LatestMetric{},
 			} {
-				if err := tx.Migrator().AddColumn(model, "ContextID"); err != nil {
-					return eris.Wrapf(err, "error altering column context_id for %t", model)
+				if err := tx.Migrator().AutoMigrate(model); err != nil {
+					return eris.Wrapf(err, "error altering table %t", model)
 				}
 			}
 

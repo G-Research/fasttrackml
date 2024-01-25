231ace6c4a1946b461724c056e693ca9156b6275
Geoff Wilson <geoff@gr-oss.io>
2024-01-24 19:04:55 -0500

    Doc fix again

--- tests/integration/golang/helpers/context.go
+++ tests/integration/golang/helpers/context.go
@@ -5,14 +5,14 @@ import (
 	"strings"
 )
 
-// ExtractContextBytes finds the metric context in decoded data, using the given key prefix.
+// ExtractContextBytes finds the metric context in decoded data, using the given key prefix,
+// and marshals to bytes.
 func ExtractContextBytes(contextPrefix string, decodedData map[string]any) ([]byte, error) {
 	contx := ExtractContext(contextPrefix, decodedData)
 	return json.Marshal(contx)
 }
 
-// ExtractContextBytes finds the metric context in the decoded data, using the given key prefix,
-// and marshals to bytes.
+// ExtractContextBytes finds the metric context in the decoded data, using the given key prefix.
 func ExtractContext(contextPrefix string, decodedData map[string]any) map[string]any {
 	contx := map[string]any{}
 	for key := range decodedData {

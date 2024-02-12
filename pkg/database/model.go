fe6fb470eb186d7603b7d61672424b2887c7f72f
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-02-12 13:43:32 +0100

    *change strategy of `default` experiment checking. adjust tests.

--- pkg/database/model.go
+++ pkg/database/model.go
@@ -9,6 +9,8 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/G-Research/fasttrackml/pkg/api/mlflow/dao/models"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -65,8 +67,8 @@ type Experiment struct {
 }
 
 // IsDefault makes check that Experiment is default.
-func (e Experiment) IsDefault() bool {
-	return e.ID != nil && *e.ID == DefaultExperimentID && e.Name == DefaultExperimentName
+func (e Experiment) IsDefault(namespace *models.Namespace) bool {
+	return e.ID != nil && namespace.DefaultExperimentID != nil && *e.ID == *namespace.DefaultExperimentID
 }
 
 type ExperimentTag struct {

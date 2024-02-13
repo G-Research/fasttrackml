5fa0b0287168db130f9672a08781378198b180f4
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-02-13 12:17:29 +0100

    *refactor `app` entity.

--- pkg/api/aim2/controller/controller.go
+++ pkg/api/aim2/controller/controller.go
@@ -1,15 +1,39 @@
 package controller
 
 import (
-	"github.com/G-Research/fasttrackml/pkg/api/aim2/service"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/service/app"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/service/dashboard"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/service/experiment"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/service/project"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/service/run"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/service/tag"
 )
 
 // Controller handles all the input HTTP requests.
 type Controller struct {
-	svc *service.Service
+	tagService        *tag.Service
+	appService        *app.Service
+	runService        *run.Service
+	projectService    *project.Service
+	dashboardService  *dashboard.Service
+	experimentService *experiment.Service
 }
 
 // NewController creates new Controller instance.
-func NewController(svc *service.Service) *Controller {
-	return &Controller{svc: svc}
+func NewController(
+	tagService *tag.Service,
+	appService *app.Service,
+	runService *run.Service,
+	projectService *project.Service,
+	dashboardService *dashboard.Service,
+	experimentService *experiment.Service,
+) *Controller {
+	return &Controller{
+		tagService:        tagService,
+		appService:        appService,
+		runService:        runService,
+		projectService:    projectService,
+		dashboardService:  dashboardService,
+		experimentService: experimentService,
+	}
 }
---
+++ pkg/api/aim2/service/run/service.go
@@ -0,0 +1,9 @@
+package run
+
+// Service provides service layer to work with `run` business logic.
+type Service struct{}
+
+// NewService creates new Service instance.
+func NewService() *Service {
+	return &Service{}
+}

7cea4dc00c85f556581b5cbb4fe52c31b0c24e7c
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-02-14 16:16:45 +0100

    [release/0.5] Fix fasttrackml python client (#907)
    Backport of #897

--- python/fasttrackml/__init__.py
+++ python/fasttrackml/__init__.py
@@ -1,15 +1,17 @@
 import contextlib
 
 import mlflow
-from fasttrackml.client import FasttrackmlClient
 from mlflow import *
 
+from .client import FasttrackmlClient
+
 del log_metric, log_metrics
-from fasttrackml.fluent import log_metric, log_metrics
+from .fluent import log_metric, log_metrics
 
 __all__ = [name for name in dir() if name in dir(mlflow)]
 __all__.append("FasttrackmlClient")
 
 with contextlib.suppress(Exception):
-    from mlflow import gateway 
+    from mlflow import gateway
+
     __all__.append("gateway")

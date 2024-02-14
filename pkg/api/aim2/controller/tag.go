65e95e9dc8bba98cdfa55820a471b6aa2319cd5e
Geoff Wilson <geoff@gr-oss.io>
2024-02-14 09:30:52 -0500

    WIP

--- pkg/api/aim2/controller/tag.go
+++ pkg/api/aim2/controller/tag.go
@@ -1,7 +1,27 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/api/response"
+	"github.com/G-Research/fasttrackml/pkg/common/api"
+	"github.com/G-Research/fasttrackml/pkg/common/middleware/namespace"
+	"github.com/gofiber/fiber/v2"
+	log "github.com/sirupsen/logrus"
+)
 
 func (c Controller) GetTags(ctx *fiber.Ctx) error {
-	return ctx.JSON([]string{})
+	ns, err := namespace.GetNamespaceFromContext(ctx.Context())
+	if err != nil {
+		return api.NewInternalError("error getting namespace from context")
+	}
+	log.Debugf("getTags namespace: %s", ns.Code)
+
+	apps, err := c.tagService.
+	if err != nil {
+		return err
+	}
+
+	resp := response.NewGetAppsResponse(apps)
+	log.Debugf("getApps response: %#v", resp)
+
+	return ctx.JSON(resp)
 }

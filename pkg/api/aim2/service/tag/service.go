aee151d9080cb87f2453a5f98f6a3ad1071db0b3
Geoff Wilson <geoff@gr-oss.io>
2024-02-15 13:28:35 -0500

    Stub implementation

--- pkg/api/aim2/service/tag/service.go
+++ pkg/api/aim2/service/tag/service.go
@@ -22,10 +22,13 @@ func NewService(tagRepository repositories.TagRepositoryProvider) *Service {
 }
 
 // GetTags returns the list of active apps.
+// TODO this is not implemented
 func (s Service) GetTags(ctx context.Context, namespace *mlflowModels.Namespace) ([]aimModels.Tag, error) {
 	tags, err := s.tagRepository.GetTagsByNamespace(ctx, namespace.ID)
 	if err != nil {
 		return nil, api.NewInternalError("unable to get active apps: %v", err)
 	}
+	// TODO remove stub data
+	tags = []aimModels.Tag{}
 	return tags, nil
 }

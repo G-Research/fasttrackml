30a178516d886a586bc883af52da98b08abbffc1
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-02-21 12:29:51 +0100

    *in progress with `/projects` endpoints refactoring.

--- pkg/api/aim2/service/project/service.go
+++ pkg/api/aim2/service/project/service.go
@@ -1,9 +1,68 @@
 package project
 
+import (
+	"context"
+	"time"
+
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/dao/dto"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/dao/models"
+	"github.com/G-Research/fasttrackml/pkg/api/aim2/dao/repositories"
+	"github.com/G-Research/fasttrackml/pkg/common/api"
+)
+
 // Service provides service layer to work with `project` business logic.
-type Service struct{}
+type Service struct {
+	runRepository        repositories.RunRepositoryProvider
+	experimentRepository repositories.ExperimentRepositoryProvider
+}
 
 // NewService creates new Service instance.
-func NewService() *Service {
-	return &Service{}
+func NewService(
+	runRepository repositories.RunRepositoryProvider,
+	experimentRepository repositories.ExperimentRepositoryProvider,
+) *Service {
+	return &Service{
+		runRepository:        runRepository,
+		experimentRepository: experimentRepository,
+	}
+}
+
+// GetProjectInformation returns project information.
+func (s Service) GetProjectInformation() (string, string) {
+	return "FastTrackML", s.runRepository.GetDB().Dialector.Name()
+}
+
+// GetProjectActivity returns project activity.
+func (s Service) GetProjectActivity(
+	ctx context.Context, namespaceID uint, tzOffset int,
+) (*dto.ProjectActivity, error) {
+	activeExperiments, err := s.experimentRepository.GetCountOfActiveExperiments(ctx, namespaceID)
+	if err != nil {
+		return nil, api.NewInternalError("error getting number of active experiments: %s", err)
+	}
+
+	runs, err := s.runRepository.GetByNamespaceID(ctx, namespaceID)
+	if err != nil {
+		return nil, api.NewInternalError("error getting runs: %s", err)
+	}
+
+	activity, numActiveRuns, numArchivedRuns := map[string]int{}, int64(0), int64(0)
+	for _, r := range runs {
+		switch {
+		case r.Status == models.StatusRunning:
+			numActiveRuns += 1
+		case r.LifecycleStage == models.LifecycleStageDeleted:
+			numArchivedRuns += 1
+		}
+		key := time.UnixMilli(r.StartTime.Int64).Add(time.Duration(-tzOffset) * time.Minute).Format("2006-01-02T15:00:00")
+		activity[key] += 1
+	}
+
+	return &dto.ProjectActivity{
+		NumRuns:         int64(len(runs)),
+		ActivityMap:     activity,
+		NumActiveRuns:   numActiveRuns,
+		NumExperiments:  activeExperiments,
+		NumArchivedRuns: numArchivedRuns,
+	}, nil
 }

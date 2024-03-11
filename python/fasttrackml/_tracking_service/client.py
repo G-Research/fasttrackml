b8a9561e9bac42753216753cac84d19116c3602f
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-03-11 14:05:09 +0100

    Fix python client to provide credentials (#996)
    Fix credential in custom_store for fasttrackml python client

--- python/fasttrackml/_tracking_service/client.py
+++ python/fasttrackml/_tracking_service/client.py
@@ -1,13 +1,14 @@
+from functools import partial
 from typing import Dict, Optional, Sequence
 
 from mlflow.entities import Param, RunTag
 from mlflow.store.tracking import GET_METRIC_HISTORY_MAX_RESULTS
+from mlflow.tracking._tracking_service import utils
 from mlflow.tracking._tracking_service.client import TrackingServiceClient
 from mlflow.tracking.metric_value_conversion_utils import (
     convert_metric_value_to_float_if_possible,
 )
 from mlflow.utils import chunk_list
-from mlflow.utils.rest_utils import MlflowHostCreds
 from mlflow.utils.time import get_current_time_millis
 from mlflow.utils.validation import MAX_METRICS_PER_BATCH
 
@@ -19,7 +20,7 @@ class FasttrackmlTrackingServiceClient(TrackingServiceClient):
 
     def __init__(self, tracking_uri):
         super().__init__(tracking_uri)
-        self.custom_store = CustomRestStore(lambda: MlflowHostCreds(self.tracking_uri))
+        self.custom_store = CustomRestStore(partial(utils._get_default_host_creds, self.tracking_uri))
 
     def log_metric(
         self,

c58553ad2c08f1c3cb86c60bb60c952ad28d73ac
Geoff Wilson <geoff@gr-oss.io>
2024-03-29 14:17:02 -0400

    Extract type from string Value column

--- pkg/api/aim2/dao/models/param.go
+++ pkg/api/aim2/dao/models/param.go
@@ -1,34 +1,28 @@
 package models
 
-import "fmt"
+import (
+	"strconv"
+	"strings"
+)
 
 // Param represents model to work with `params` table.
 type Param struct {
 	Key        string   `gorm:"type:varchar(250);not null;primaryKey"`
 	Value      string   `gorm:"type:varchar(500);not null"`
-	ValueInt   *int64   `gorm:"type:bigint"`
-	ValueFloat *float64 `gorm:"type:float"`
 	RunID      string   `gorm:"column:run_uuid;not null;primaryKey;index"`
 }
 
-// ValueString returns the value held by this Param as a string
-func (p Param) ValueString() string {
-	if p.ValueInt != nil {
-		return fmt.Sprintf("%v", *p.ValueInt)
-	} else if p.ValueFloat != nil {
-		return fmt.Sprintf("%v", *p.ValueFloat)
-	} else {
-		return p.Value
+// ValueTyped returns value held by this Param as any but with correct underlying type.
+func (p Param) ValueTyped() any {
+	if strings.Contains(p.Value, ".") {
+		floatVal, err := strconv.ParseFloat(p.Value, 64)
+		if err == nil {
+			return floatVal
+		}
 	}
-}
-
-// ValueAny returns the value held by this Param as any
-func (p Param) ValueAny() any {
-	if p.ValueInt != nil {
-		return *p.ValueInt
-	} else if p.ValueFloat != nil {
-		return *p.ValueFloat
-	} else {
-		return p.Value
+	intVal, err := strconv.ParseInt(p.Value, 10, 64)
+	if err == nil {
+		return intVal
 	}
+	return p.Value
 }

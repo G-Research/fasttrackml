5ecf56dcf6da06837e35ee64b3fa598bbf3d27cf
Geoff Wilson <geoff@gr-oss.io>
2024-04-01 15:07:59 -0400

    Unit test for ValueTyped

---
+++ pkg/api/aim2/dao/models/param_test.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValueTyped(t *testing.T) {
+	tests := []struct {
+		name  string
+		param Param
+		want  any
+	}{
+		{
+			name:  "IntegerValue",
+			param: Param{Value: "123"},
+			want:  int64(123),
+		},
+		{
+			name:  "FloatValue",
+			param: Param{Value: "123.45"},
+			want:  float64(123.45),
+		},
+		{
+			name:  "StringValue",
+			param: Param{Value: "abc"},
+			want:  "abc",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valueTyped := tt.param.ValueTyped()
+			assert.Equal(t, tt.want, valueTyped)
+			assert.IsType(t, tt.want, valueTyped)
+		})
+	}
+}

ad4d005743eda05b1cbf7ba0cc4de7e369b84bcd
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-04-26 09:23:55 +0200

    *revert `experiments` property back for `/project/params` endpoint.

--- tests/integration/golang/aim/project/get_project_params_test.go
+++ tests/integration/golang/aim/project/get_project_params_test.go
@@ -94,7 +94,7 @@ func (s *GetProjectParamsTestSuite) Test_Ok() {
 		{
 			name: "RequestProjectParamsFilteredByExistingExperiment",
 			request: map[any]any{
-				"experiment_names": s.DefaultExperiment.Name,
+				"experiments": *s.DefaultExperiment.ID,
 			},
 			response: response.ProjectParamsResponse{
 				Metric: map[string][]fiber.Map{
@@ -119,7 +119,7 @@ func (s *GetProjectParamsTestSuite) Test_Ok() {
 		{
 			name: "RequestProjectParamsFilteredByNotExistingExperiment",
 			request: map[any]any{
-				"experiment_names": "not-existing-experiment",
+				"experiments": 999,
 			},
 			response: response.ProjectParamsResponse{
 				Metric: map[string][]fiber.Map{},

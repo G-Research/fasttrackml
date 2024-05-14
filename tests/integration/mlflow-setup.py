f8cb4d41860e68b9d5ee5f77ed3f3ef1734f16fa
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-05-14 17:54:43 +0200

    Create integration tests to test FastTrackML over MLFlow native database (#1199)

---
+++ tests/integration/mlflow-setup.py
@@ -0,0 +1,17 @@
+import os
+from random import randint, random
+
+import mlflow
+from mlflow import log_metric, log_param
+
+mlflow.set_tracking_uri(os.getenv("BACKEND_STORE_URI"))
+mlflow.set_experiment("mlflow-experiment")
+
+if __name__ == "__main__":
+    # Log a parameter (key-value pair)
+    log_param("param1", randint(0, 100))
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", random())
+    log_metric("foo", random() + 1)
+    log_metric("foo", random() + 2)

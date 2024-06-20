c59d10a95a4b23bf917c0baa93caf9c109307c03
Geoff Wilson <geoff@gr-oss.io>
2024-06-20 15:11:20 -0400

    Integration tests for crud endpoints

--- tests/integration/golang/fixtures/shared_tag.go
+++ tests/integration/golang/fixtures/shared_tag.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/rotisserie/eris"
 	"gorm.io/gorm"
 
@@ -46,8 +47,30 @@ func (f SharedTagFixtures) GetByRunID(ctx context.Context, runID string) ([]mode
 	return run.SharedTags, nil
 }
 
+// GetTag returns SharedTag by requested ID.
+func (f SharedTagFixtures) GetTag(ctx context.Context, id uuid.UUID) (*models.SharedTag, error) {
+	var sharedTag models.SharedTag
+	if err := f.db.WithContext(ctx).Where(
+		models.SharedTag{ID: id},
+	).First(&sharedTag).Error; err != nil {
+		return nil, eris.Wrapf(err, "error getting tag by id: %s", id)
+	}
+	return &sharedTag, nil
+}
+
+// GetTags returns SharedTag list.
+func (f SharedTagFixtures) GetTags(ctx context.Context) ([]models.SharedTag, error) {
+	var tags []models.SharedTag
+	if err := f.db.WithContext(ctx).Model(
+		models.SharedTag{},
+	).Find(&tags).Error; err != nil {
+		return nil, eris.Wrapf(err, "error getting tags")
+	}
+	return tags, nil
+}
+
 // Associate creates a tag relationship between run and tag.
-func (f SharedTagFixtures) Associate(ctx context.Context, runID, tagID string) error {
+func (f SharedTagFixtures) Associate(ctx context.Context, tagID, runID string) error {
 	if err := f.db.WithContext(ctx).Exec(`
 		INSERT INTO run_shared_tags (run_id, shared_tag_id) 
 		VALUES (?, ?) 
